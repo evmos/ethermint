4171212fcee342e629375b47c8bbdd0af79ee09b
Aleksandr Bezobchuk <alex@tendermint.com>
2018-10-04 16:12:13 -0400

    Register Account interface with amino in types

--- types/wire.go
+++ types/wire.go
@@ -3,6 +3,7 @@ package types
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/wire"
+	"github.com/cosmos/cosmos-sdk/x/auth"
 )
 
 var typesCodec = wire.NewCodec()
@@ -15,6 +16,7 @@ func init() {
 // codec.
 func RegisterWire(codec *wire.Codec) {
 	sdk.RegisterWire(codec)
+	codec.RegisterInterface((*auth.Account)(nil), nil)
 	codec.RegisterConcrete(&Transaction{}, "types/Transaction", nil)
 	codec.RegisterConcrete(&Account{}, "types/Account", nil)
 }
