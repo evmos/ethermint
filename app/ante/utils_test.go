799d80befd006798ee7840c0a4890c0546cec02c
huangyi <huang@crypto.com>
2021-06-21 10:01:59 +0800

    add ValidateMemoDecorator to ante handler of evm tx
    fixes #149

--- app/ante/utils_test.go
+++ app/ante/utils_test.go
@@ -73,6 +73,13 @@ func TestAnteTestSuite(t *testing.T) {
 func (suite *AnteTestSuite) CreateTestTx(
 	msg *evmtypes.MsgEthereumTx, priv cryptotypes.PrivKey, accNum uint64,
 ) authsigning.Tx {
+	return suite.CreateTestTxBuilder(msg, priv, accNum).GetTx()
+}
+
+// CreateTestTxBuilder is a helper function to create a tx builder given multiple inputs.
+func (suite *AnteTestSuite) CreateTestTxBuilder(
+	msg *evmtypes.MsgEthereumTx, priv cryptotypes.PrivKey, accNum uint64,
+) client.TxBuilder {
 
 	option, err := codectypes.NewAnyWithValue(&evmtypes.ExtensionOptionsEthereumTx{})
 	suite.Require().NoError(err)
@@ -125,7 +132,7 @@ func (suite *AnteTestSuite) CreateTestTx(
 	err = suite.txBuilder.SetSignatures(sigsV2...)
 	suite.Require().NoError(err)
 
-	return suite.txBuilder.GetTx()
+	return suite.txBuilder
 }
 
 func newTestAddrKey() (common.Address, cryptotypes.PrivKey) {
