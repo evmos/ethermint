4997bede5ec9e62cee5decf10b2e7830adf4a1a7
Federico Kunze <31522760+fedekunze@users.noreply.github.com>
2021-06-24 03:57:31 -0400

    Update ethereum/rpc/types/types.go

--- ethereum/rpc/types/types.go
+++ ethereum/rpc/types/types.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"fmt"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
