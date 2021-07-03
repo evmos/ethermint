0fc3472705b617620687b32b99fbc305c6fc506c
crypto-facs <84574577+crypto-facs@users.noreply.github.com>
2021-07-03 06:21:17 -0400

    rpc: fix parent hash format (#222)

--- ethereum/rpc/types/utils.go
+++ ethereum/rpc/types/utils.go
@@ -178,7 +178,7 @@ func FormatBlock(
 	return map[string]interface{}{
 		"number":           hexutil.Uint64(header.Height),
 		"hash":             hexutil.Bytes(header.Hash()),
-		"parentHash":       hexutil.Bytes(header.LastBlockID.Hash),
+		"parentHash":       common.BytesToHash(header.LastBlockID.Hash.Bytes()),
 		"nonce":            ethtypes.BlockNonce{},   // PoW specific
 		"sha3Uncles":       ethtypes.EmptyUncleHash, // No uncles in Tendermint
 		"logsBloom":        bloom,
