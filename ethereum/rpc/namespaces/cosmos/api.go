fe1903c58616e0d400bee9f6c34244c15b2bc08d
Akash Khosla <me@akashkhosla.com>
2021-07-13 01:05:35 -0400

    rpc: add SignDirect

--- ethereum/rpc/namespaces/cosmos/api.go
+++ ethereum/rpc/namespaces/cosmos/api.go
@@ -1,8 +1,11 @@
 package cosmos
 
 import (
+	"encoding/hex"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/tendermint/tendermint/libs/log"
 )
 
@@ -72,3 +75,65 @@ func (api *WalletConnectAPI) GetAccounts() ([]AccountsResponse, error) {
 
 	return accs, nil
 }
+
+type SignDirectRequest struct {
+	SignerAddress sdk.AccAddress  `json:"signerAddress"`
+	SignDoc       txtypes.SignDoc `json:"signDoc"`
+}
+type SignDirectResponse struct {
+	Signature string          `json:"signature"`
+	SignDoc   txtypes.SignDoc `json:"signDoc"`
+}
+
+// This method returns a signature for the provided document to be signed
+// targetting the requested signer address corresponding to the keypair returned
+// by the account data.
+func (api *WalletConnectAPI) SignDirect(req SignDirectRequest) (SignDirectResponse, error) {
+	api.logger.Debug("cosmos_signDirect")
+
+	_, err := api.clientCtx.Keyring.KeyByAddress(req.SignerAddress)
+	if err != nil {
+		api.logger.Error("failed to find key in keyring", "address", req.SignerAddress.String())
+		return SignDirectResponse{}, err
+	}
+
+	signBytes, err := req.SignDoc.Marshal()
+	if err != nil {
+		api.logger.Error("failed to unpack tx data")
+		return SignDirectResponse{}, err
+	}
+	signature, _, err := api.clientCtx.Keyring.SignByAddress(req.SignerAddress, signBytes)
+	if err != nil {
+		api.logger.Error("keyring.SignByAddress failed", "address", req.SignerAddress.String())
+		return SignDirectResponse{}, err
+	}
+	return SignDirectResponse{
+		Signature: hex.EncodeToString(signature),
+		SignDoc: txtypes.SignDoc{
+			ChainId: "test",
+		},
+	}, nil
+}
+
+// // Sign signs the provided data using the private key of address via Geth's signature standard.
+// func (e *PublicAPI) Sign(address common.Address, data hexutil.Bytes) (hexutil.Bytes, error) {
+// 	e.logger.Debug("eth_sign", "address", address.Hex(), "data", common.Bytes2Hex(data))
+
+// 	from := sdk.AccAddress(address.Bytes())
+
+// 	_, err := e.clientCtx.Keyring.KeyByAddress(from)
+// 	if err != nil {
+// 		e.logger.Error("failed to find key in keyring", "address", address.String())
+// 		return nil, fmt.Errorf("%s; %s", keystore.ErrNoMatch, err.Error())
+// 	}
+
+// 	// Sign the requested hash with the wallet
+// 	signature, _, err := e.clientCtx.Keyring.SignByAddress(from, data)
+// 	if err != nil {
+// 		e.logger.Error("keyring.SignByAddress failed", "address", address.Hex())
+// 		return nil, err
+// 	}
+
+// 	signature[64] += 27 // Transform V from 0/1 to 27/28 according to the yellow paper
+// 	return signature, nil
+// }
