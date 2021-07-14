e09bf23bd0badb8712b5268b8d316cd88555c736
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2021-07-14 05:13:55 -0400

    evm: move `ChainConfig` to `Params` (#266)
    * evm: move ChainConfig to Params
    
    * fixes
    
    * fix test

--- x/evm/keeper/params.go
+++ x/evm/keeper/params.go
@@ -16,23 +16,3 @@ func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramSpace.SetParamSet(ctx, &params)
 }
-
-// GetChainConfig gets block height from block consensus hash
-func (k Keeper) GetChainConfig(ctx sdk.Context) (types.ChainConfig, bool) {
-	store := ctx.KVStore(k.storeKey)
-	bz := store.Get(types.KeyPrefixChainConfig)
-	if len(bz) == 0 {
-		return types.ChainConfig{}, false
-	}
-
-	var config types.ChainConfig
-	k.cdc.MustUnmarshal(bz, &config)
-	return config, true
-}
-
-// SetChainConfig sets the mapping from block consensus hash to block height
-func (k Keeper) SetChainConfig(ctx sdk.Context, config types.ChainConfig) {
-	store := ctx.KVStore(k.storeKey)
-	bz := k.cdc.MustMarshal(&config)
-	store.Set(types.KeyPrefixChainConfig, bz)
-}
