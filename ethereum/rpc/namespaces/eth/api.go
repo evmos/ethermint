26f36bee32b73666968c0d0bb4b316f2b8a9c0bc
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2021-07-15 05:40:01 -0400

    Apply suggestions from code review

--- ethereum/rpc/namespaces/eth/api.go
+++ ethereum/rpc/namespaces/eth/api.go
@@ -273,12 +273,12 @@ func (e *PublicAPI) GetBlockTransactionCountByHash(hash common.Hash) *hexutil.Ui
 
 	resBlock, err := e.clientCtx.Client.BlockByHash(e.ctx, hash.Bytes())
 	if err != nil {
-		e.logger.WithError(err).Debugln("block not found", "hash", hash.Hex())
+		e.logger.Debug("block not found", "hash", hash.Hex(), "error", err.Error())
 		return nil
 	}
 
 	if resBlock.Block == nil {
-		e.logger.Debugln("block not found", "hash", hash.Hex())
+		e.logger.Debug("block not found", "hash", hash.Hex())
 		return nil
 	}
 
@@ -288,15 +288,15 @@ func (e *PublicAPI) GetBlockTransactionCountByHash(hash common.Hash) *hexutil.Ui
 
 // GetBlockTransactionCountByNumber returns the number of transactions in the block identified by number.
 func (e *PublicAPI) GetBlockTransactionCountByNumber(blockNum rpctypes.BlockNumber) *hexutil.Uint {
-	e.logger.Debug("eth_getBlockTransactionCountByNumber", "block number", blockNum.Int64())
+	e.logger.Debug("eth_getBlockTransactionCountByNumber", "height", blockNum.Int64())
 	resBlock, err := e.clientCtx.Client.Block(e.ctx, blockNum.TmHeight())
 	if err != nil {
-		e.logger.WithError(err).Debugln("block not found", "height", blockNum.Int64())
+		e.logger.Debug("block not found", "height", blockNum.Int64(), "error", err.Error())
 		return nil
 	}
 
 	if resBlock.Block == nil {
-		e.logger.Debugln("block not found", "height", blockNum.Int64())
+		e.logger.Debug("block not found", "height", blockNum.Int64())
 		return nil
 	}
 
@@ -681,7 +681,7 @@ func (e *PublicAPI) GetTransactionByBlockHashAndIndex(hash common.Hash, idx hexu
 	}
 
 	if resBlock.Block == nil {
-		e.logger.Debugln("block not found", "hash", hash.Hex())
+		e.logger.Debug("block not found", "hash", hash.Hex())
 		return nil, nil
 	}
 
@@ -724,7 +724,7 @@ func (e *PublicAPI) GetTransactionByBlockNumberAndIndex(blockNum rpctypes.BlockN
 	}
 
 	if resBlock.Block == nil {
-		e.logger.Debugln("block not found", "height", blockNum.Int64())
+		e.logger.Debug("block not found", "height", blockNum.Int64())
 		return nil, nil
 	}
 
