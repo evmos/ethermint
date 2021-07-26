dce20db3e7c979a71a45fbf19db79b91b2bc9e65
Guillermo <guillermo.paoletti@gmail.com>
2021-07-26 18:50:39 +0200

    SetGasPrice call

--- ethereum/rpc/namespaces/miner/api.go
+++ ethereum/rpc/namespaces/miner/api.go
@@ -2,6 +2,8 @@ package miner
 
 import (
 	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/server/config"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/tendermint/tendermint/libs/log"
@@ -31,10 +33,24 @@ func (api *API) SetEtherbase(etherbase common.Address) bool {
 
 // SetGasPrice sets the minimum accepted gas price for the miner.
 func (api *API) SetGasPrice(gasPrice hexutil.Big) bool {
-	// api.e.lock.Lock()
-	// api.e.gasPrice = (*big.Int)(&gasPrice)
-	// api.e.lock.Unlock()
-
-	// api.e.txPool.SetGasPrice((*big.Int)(&gasPrice))
-	return false
+	api.logger.Info(api.ctx.Viper.ConfigFileUsed())
+	appConf, err := config.ParseConfig(api.ctx.Viper)
+	if err != nil {
+		// TODO: fix this error format
+		api.logger.Error("failed to parse %s: %w", api.ctx.Viper.ConfigFileUsed(), err)
+		return false
+	}
+	// TODO: should this value be wei?
+	coinsValue := gasPrice.ToInt().String()
+	unit := "aphoton"
+	c, err := sdk.ParseDecCoins(coinsValue + unit)
+	if err != nil {
+		// TODO: fix this error format
+		api.logger.Error("failed to parse coins %s, %s: %w", coinsValue, unit, err)
+		return false
+	}
+	appConf.SetMinGasPrices(c)
+	config.WriteConfigFile(api.ctx.Viper.ConfigFileUsed(), appConf)
+	api.logger.Info("Your configuration file was modified. Please RESTART your node.", "value", coinsValue+unit)
+	return true
 }
