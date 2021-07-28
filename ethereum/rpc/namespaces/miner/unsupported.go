37c1950bbccfeed7e3bf04e23e390bc5cb099d52
ramacarlucho <ramirocarlucho@gmail.com>
2021-07-28 11:43:10 -0300

    change log level

--- ethereum/rpc/namespaces/miner/unsupported.go
+++ ethereum/rpc/namespaces/miner/unsupported.go
@@ -10,7 +10,7 @@ import (
 // Unsupported in Ethermint
 func (api *API) GetHashrate() uint64 {
 	api.logger.Debug("miner_getHashrate")
-	api.logger.Info("Unsupported rpc function: miner_getHashrate")
+	api.logger.Debug("Unsupported rpc function: miner_getHashrate")
 	return 0
 }
 
@@ -18,7 +18,7 @@ func (api *API) GetHashrate() uint64 {
 // Unsupported in Ethermint
 func (api *API) SetExtra(extra string) (bool, error) {
 	api.logger.Debug("miner_setExtra")
-	api.logger.Info("Unsupported rpc function: miner_setExtra")
+	api.logger.Debug("Unsupported rpc function: miner_setExtra")
 	return false, errors.New("unsupported rpc function: miner_setExtra")
 }
 
@@ -26,7 +26,7 @@ func (api *API) SetExtra(extra string) (bool, error) {
 // Unsupported in Ethermint
 func (api *API) SetGasLimit(gasLimit hexutil.Uint64) bool {
 	api.logger.Debug("miner_setGasLimit")
-	api.logger.Info("Unsupported rpc function: miner_setGasLimit")
+	api.logger.Debug("Unsupported rpc function: miner_setGasLimit")
 	return false
 }
 
@@ -38,7 +38,7 @@ func (api *API) SetGasLimit(gasLimit hexutil.Uint64) bool {
 // Unsupported in Ethermint
 func (api *API) Start(threads *int) error {
 	api.logger.Debug("miner_start")
-	api.logger.Info("Unsupported rpc function: miner_start")
+	api.logger.Debug("Unsupported rpc function: miner_start")
 	return errors.New("unsupported rpc function: miner_start")
 }
 
@@ -47,5 +47,5 @@ func (api *API) Start(threads *int) error {
 // Unsupported in Ethermint
 func (api *API) Stop() {
 	api.logger.Debug("miner_stop")
-	api.logger.Info("Unsupported rpc function: miner_stop")
+	api.logger.Debug("Unsupported rpc function: miner_stop")
 }
