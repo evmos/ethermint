a5ab608336276e99ba18556ffbdaee9ccdc9ca93
yihuang <huang@crypto.com>
2021-08-30 11:14:58 +0800

    evm: benchmark contract execution (#436)
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- x/evm/keeper/keeper_test.go
+++ x/evm/keeper/keeper_test.go
@@ -1,9 +1,13 @@
 package keeper_test
 
 import (
+	_ "embed"
+	"encoding/json"
+	"math/big"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
@@ -20,18 +24,45 @@ import (
 	"github.com/tharsis/ethermint/app"
 	"github.com/tharsis/ethermint/crypto/ethsecp256k1"
 	"github.com/tharsis/ethermint/encoding"
+	"github.com/tharsis/ethermint/server/config"
 	"github.com/tharsis/ethermint/tests"
 	ethermint "github.com/tharsis/ethermint/types"
 	"github.com/tharsis/ethermint/x/evm/types"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	ethcmn "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	ethcrypto "github.com/ethereum/go-ethereum/crypto"
 
 	abci "github.com/tendermint/tendermint/abci/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
+
+var (
+	//go:embed ERC20Contract.json
+	compiledContractJSON []byte
+	ContractBin          []byte
+	ContractABI          abi.ABI
+)
+
+func init() {
+	var tmp struct {
+		Abi string
+		Bin string
+	}
+	err := json.Unmarshal(compiledContractJSON, &tmp)
+	if err != nil {
+		panic(err)
+	}
+	ContractBin = common.FromHex(tmp.Bin)
+	err = json.Unmarshal([]byte(tmp.Abi), &ContractABI)
+	if err != nil {
+		panic(err)
+	}
+}
 
 var testTokens = sdk.NewIntWithDecimal(1000, 18)
 
@@ -52,18 +83,19 @@ type KeeperTestSuite struct {
 	signer   keyring.Signer
 }
 
-func (suite *KeeperTestSuite) SetupTest() {
+/// DoSetupTest setup test environment, it uses`require.TestingT` to support both `testing.T` and `testing.B`.
+func (suite *KeeperTestSuite) DoSetupTest(t require.TestingT) {
 	checkTx := false
 
 	// account key
 	priv, err := ethsecp256k1.GenerateKey()
-	suite.Require().NoError(err)
+	require.NoError(t, err)
 	suite.address = ethcmn.BytesToAddress(priv.PubKey().Address().Bytes())
 	suite.signer = tests.NewSigner(priv)
 
 	// consensus key
 	priv, err = ethsecp256k1.GenerateKey()
-	suite.Require().NoError(err)
+	require.NoError(t, err)
 	suite.consAddress = sdk.ConsAddress(priv.PubKey().Address())
 
 	suite.app = app.Setup(checkTx)
@@ -89,9 +121,9 @@ func (suite *KeeperTestSuite) SetupTest() {
 	valAddr := sdk.ValAddress(suite.address.Bytes())
 	validator, err := stakingtypes.NewValidator(valAddr, priv.PubKey(), stakingtypes.Description{})
 	err = suite.app.StakingKeeper.SetValidatorByConsAddr(suite.ctx, validator)
-	suite.Require().NoError(err)
+	require.NoError(t, err)
 	err = suite.app.StakingKeeper.SetValidatorByConsAddr(suite.ctx, validator)
-	suite.Require().NoError(err)
+	require.NoError(t, err)
 	suite.app.StakingKeeper.SetValidator(suite.ctx, validator)
 
 	encodingConfig := encoding.MakeConfig(app.ModuleBasics)
@@ -101,13 +133,20 @@ func (suite *KeeperTestSuite) SetupTest() {
 
 	// mint some tokens to coinbase address
 	_, bankKeeper := suite.initKeepersWithmAccPerms()
-	ctx := sdk.WrapSDKContext(suite.ctx)
-	rsp, err := suite.queryClient.Params(ctx, &types.QueryParamsRequest{})
-	suite.Require().NoError(err)
-	initCoin := sdk.NewCoins(sdk.NewCoin(rsp.Params.EvmDenom, testTokens))
-
+	require.NoError(t, err)
+	initCoin := sdk.NewCoins(sdk.NewCoin(suite.EvmDenom(), testTokens))
 	err = simapp.FundAccount(bankKeeper, suite.ctx, acc.GetAddress(), initCoin)
-	suite.Require().NoError(err)
+	require.NoError(t, err)
+}
+
+func (suite *KeeperTestSuite) SetupTest() {
+	suite.DoSetupTest(suite.T())
+}
+
+func (suite *KeeperTestSuite) EvmDenom() string {
+	ctx := sdk.WrapSDKContext(suite.ctx)
+	rsp, _ := suite.queryClient.Params(ctx, &types.QueryParamsRequest{})
+	return rsp.Params.EvmDenom
 }
 
 // Commit and begin new block
@@ -144,6 +183,46 @@ func (suite *KeeperTestSuite) initKeepersWithmAccPerms() (authkeeper.AccountKeep
 	)
 
 	return authKeeper, keeper
+}
+
+// DeployTestContract deploy a test erc20 contract and returns the contract address
+func (suite *KeeperTestSuite) DeployTestContract(t require.TestingT, owner common.Address, supply *big.Int) common.Address {
+	ctx := sdk.WrapSDKContext(suite.ctx)
+	chainID := suite.app.EvmKeeper.ChainID()
+
+	ctorArgs, err := ContractABI.Pack("", owner, supply)
+	require.NoError(t, err)
+
+	data := append(ContractBin, ctorArgs...)
+	args, err := json.Marshal(&types.CallArgs{
+		From: &suite.address,
+		Data: (*hexutil.Bytes)(&data),
+	})
+	require.NoError(t, err)
+
+	res, err := suite.queryClient.EstimateGas(ctx, &types.EthCallRequest{
+		Args:   args,
+		GasCap: uint64(config.DefaultGasCap),
+	})
+	require.NoError(t, err)
+
+	nonce := suite.app.EvmKeeper.GetNonce(suite.address)
+	erc20DeployTx := types.NewTxContract(
+		chainID,
+		nonce,
+		nil,     // amount
+		res.Gas, // gasLimit
+		nil,     // gasPrice
+		data,    // input
+		nil,     // accesses
+	)
+	erc20DeployTx.From = suite.address.Hex()
+	err = erc20DeployTx.Sign(ethtypes.LatestSignerForChainID(chainID), suite.signer)
+	require.NoError(t, err)
+	rsp, err := suite.app.EvmKeeper.EthereumTx(ctx, erc20DeployTx)
+	require.NoError(t, err)
+	require.Empty(t, rsp.VmError)
+	return crypto.CreateAddress(suite.address, nonce)
 }
 
 func TestKeeperTestSuite(t *testing.T) {
