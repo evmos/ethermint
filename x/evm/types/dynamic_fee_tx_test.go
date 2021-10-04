2491701a7641c6eb5c5058458a74d8d4eff88946
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2021-10-04 15:21:43 +0200

    Start get and set signature values tests

--- x/evm/types/dynamic_fee_tx_test.go
+++ x/evm/types/dynamic_fee_tx_test.go
@@ -5,15 +5,93 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
+	"github.com/tharsis/ethermint/tests"
 	"github.com/tharsis/ethermint/x/evm/types"
 )
+
+func TestGetRawSignatureValues(t *testing.T) {
+	hundredInt := sdk.NewInt(100)
+	validAddr := tests.GenerateAddress().Hex()
+
+	testCases := []struct {
+		name    string
+		tx      types.DynamicFeeTx
+		expPass bool
+	}{
+		{
+			"success",
+			types.DynamicFeeTx{
+				GasTipCap: &hundredInt,
+				GasFeeCap: &hundredInt,
+				Amount:    &hundredInt,
+				To:        validAddr,
+				ChainID:   &hundredInt,
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		values := tc.tx.GetRawSignatureValues()
+
+		if tc.expPass {
+			require.NoError(t, values, tc.name)
+			continue
+		}
+
+		require.Error(t, values, tc.name)
+	}
+}
+
+// func TestSetSignatureValues(t *testing.T) {
+// 	hundredInt := sdk.NewInt(100)
+// 	validAddr := tests.GenerateAddress().Hex()
+// 	chainID := big.NewInt(0)
+
+// 	testCases := []struct {
+// 		name    string
+// 		tx      types.DynamicFeeTx
+// 		chainID *big.Int
+// 		r       *big.Int
+// 		v       *big.Int
+// 		s       *big.Int
+// 		expPass bool
+// 	}{
+// 		{
+// 			"chainID",
+// 			types.DynamicFeeTx{
+// 				GasTipCap: &hundredInt,
+// 				GasFeeCap: &hundredInt,
+// 				Amount:    &hundredInt,
+// 				To:        validAddr,
+// 				ChainID:   chainID,
+// 			},
+// 			chainID,
+// 			big.NewInt(0),
+// 			big.NewInt(1),
+// 			big.NewInt(2),
+// 			true,
+// 		},
+// 	}
+
+// for _, tc := range testCases {
+// 	err := tc.tx.SetSignatureValues(tc.chainID, tc.v, tc.r, tc.s)
+
+// 	if tc.expPass {
+// 		require.NoError(t, err, tc.name)
+// 		continue
+// 	}
+
+// 	require.Error(t, err, tc.name)
+// }
+// }
 
 func TestDynamicFeeTxValidate(t *testing.T) {
 	hundredInt := sdk.NewInt(100)
 	zeroInt := sdk.ZeroInt()
 	minusOneInt := sdk.NewInt(-1)
-	invalidAddress := "123456"
-	validAddress := "0xD115BFFAbbdd893A6f7ceA402e7338643Ced44a6"
+	invalidAddr := "123456"
+	validAddr := tests.GenerateAddress().Hex()
 
 	testCases := []struct {
 		name     string
@@ -78,7 +156,7 @@ func TestDynamicFeeTxValidate(t *testing.T) {
 				GasTipCap: &hundredInt,
 				GasFeeCap: &hundredInt,
 				Amount:    &hundredInt,
-				To:        invalidAddress,
+				To:        invalidAddr,
 			},
 			true,
 		},
@@ -88,7 +166,7 @@ func TestDynamicFeeTxValidate(t *testing.T) {
 				GasTipCap: &hundredInt,
 				GasFeeCap: &hundredInt,
 				Amount:    &hundredInt,
-				To:        validAddress,
+				To:        validAddr,
 				ChainID:   nil,
 			},
 			true,
@@ -99,7 +177,7 @@ func TestDynamicFeeTxValidate(t *testing.T) {
 				GasTipCap: &hundredInt,
 				GasFeeCap: &hundredInt,
 				Amount:    &hundredInt,
-				To:        validAddress,
+				To:        validAddr,
 				ChainID:   &hundredInt,
 			},
 			false,
