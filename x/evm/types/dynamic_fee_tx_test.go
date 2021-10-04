f99cca21217c4f376be5c1a50ffb5e643eb2c98b
Federico Kunze <federico.kunze94@gmail.com>
2021-10-04 11:40:30 +0200

    evm: unit tests

---
+++ x/evm/types/dynamic_fee_tx_test.go
@@ -0,0 +1,76 @@
+package types_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"github.com/tharsis/ethermint/x/evm/types"
+)
+
+func TestDynamicFeeTxValidate(t *testing.T) {
+	hundredInt := sdk.NewInt(100)
+	zeroInt := sdk.ZeroInt()
+	minusOneInt := sdk.NewInt(-1)
+
+	testCases := []struct {
+		name     string
+		tx       types.DynamicFeeTx
+		expError bool
+	}{
+		{
+			"empty",
+			types.DynamicFeeTx{},
+			true,
+		},
+		{
+			"gas tip cap is nil",
+			types.DynamicFeeTx{
+				GasTipCap: nil,
+			},
+			true,
+		},
+		{
+			"gas fee cap is nil",
+			types.DynamicFeeTx{
+				GasTipCap: &zeroInt,
+			},
+			true,
+		},
+		{
+			"gas tip cap is negative",
+			types.DynamicFeeTx{
+				GasTipCap: &minusOneInt,
+				GasFeeCap: &zeroInt,
+			},
+			true,
+		},
+		{
+			"gas tip cap is negative",
+			types.DynamicFeeTx{
+				GasTipCap: &zeroInt,
+				GasFeeCap: &minusOneInt,
+			},
+			true,
+		},
+		{
+			"gas fee cap < gas tip cap",
+			types.DynamicFeeTx{
+				GasTipCap: &hundredInt,
+				GasFeeCap: &zeroInt,
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		err := tc.tx.Validate()
+
+		if tc.expError {
+			require.Error(t, err, tc.name)
+			continue
+		}
+
+		require.NoError(t, err, tc.name)
+	}
+}
