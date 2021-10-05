386943f4fbac1dd252eaf9053fdc6f0178f79908
yihuang <huang@crypto.com>
2021-10-05 18:26:31 +0800

    evm, rpc:  fix tx log attribute value is not parsable by some client (#615)
    * Problem: tx log attribute value not parsable by some client
    
    Closes: #614
    
    Solution:
    - encode the value to json string rather than bytes
    
    Apply suggestions from code review
    
    * rm cdc and changelog
    
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
    Co-authored-by: Federico Kunze <federico.kunze94@gmail.com>

--- rpc/ethereum/backend/backend.go
+++ rpc/ethereum/backend/backend.go
@@ -400,7 +400,8 @@ func (e *EVMBackend) GetTransactionLogs(txHash common.Hash) ([]*ethtypes.Log, er
 	if err != nil {
 		return nil, err
 	}
-	return TxLogsFromEvents(e.clientCtx.Codec, tx.TxResult.Events), nil
+
+	return TxLogsFromEvents(tx.TxResult.Events)
 }
 
 // PendingTransactions returns the transactions that are in the transaction pool
@@ -433,7 +434,11 @@ func (e *EVMBackend) GetLogsByHeight(height *int64) ([][]*ethtypes.Log, error) {
 
 	blockLogs := [][]*ethtypes.Log{}
 	for _, txResult := range blockRes.TxsResults {
-		logs := TxLogsFromEvents(e.clientCtx.Codec, txResult.Events)
+		logs, err := TxLogsFromEvents(txResult.Events)
+		if err != nil {
+			return nil, err
+		}
+
 		blockLogs = append(blockLogs, logs)
 	}
 
