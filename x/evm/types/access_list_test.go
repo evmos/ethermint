7cc3ed7e386963d59428e4e66d51cacf5b83fde2
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2021-10-05 19:03:03 +0200

    Add AccessList tests

--- x/evm/types/access_list_test.go
+++ x/evm/types/access_list_test.go
@@ -3,13 +3,39 @@ package types
 import (
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/require"
 )
 
+func TestNewAccessList(t *testing.T) {
+	testCases := []struct {
+		name          string
+		ethAccessList *ethtypes.AccessList
+		expAl         AccessList
+	}{
+		{
+			"ethAccessList is nil",
+			nil,
+			nil,
+		},
+		{
+			"non-empty ethAccessList",
+			&ethtypes.AccessList{{Address: addr, StorageKeys: []common.Hash{{0}}}},
+			AccessList{{Address: addr.Hex(), StorageKeys: []string{common.Hash{}.Hex()}}},
+		},
+	}
+	for _, tc := range testCases {
+		al := NewAccessList(tc.ethAccessList)
+
+		require.Equal(t, tc.expAl, al)
+	}
+}
+
 func TestAccessListToEthAccessList(t *testing.T) {
-	al := AccessList{}
+	ethAccessList := ethtypes.AccessList{{Address: addr, StorageKeys: []common.Hash{{0}}}}
+	al := NewAccessList(&ethAccessList)
 	actual := al.ToEthAccessList()
 
-	require.Equal(t, ethtypes.AccessList{}, actual)
+	require.Equal(t, &ethAccessList, actual)
 }
