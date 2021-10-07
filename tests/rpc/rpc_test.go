33816500d627ebe2a887705a93beafe209fe5cec
jay tseng <jay.tseng@crypto.com>
2021-10-06 21:56:17 -0400

    fix TestEth_EstimateGas_ContractDeployment

--- tests/rpc/rpc_test.go
+++ tests/rpc/rpc_test.go
@@ -837,7 +837,7 @@ func TestEth_EstimateGas_ContractDeployment(t *testing.T) {
 	err := json.Unmarshal(rpcRes.Result, &gas)
 	require.NoError(t, err, string(rpcRes.Result))
 
-	require.Equal(t, "0x1c2c4", gas.String())
+	require.Equal(t, "0x1879c", gas.String())
 }
 
 func TestEth_ExportAccount_WithStorage(t *testing.T) {
