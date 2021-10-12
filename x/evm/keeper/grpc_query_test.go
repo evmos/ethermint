caabc5eb3a8b86f86812cf12b02d415a1091c1ae
jay tseng <jay.tseng@crypto.com>
2021-10-12 15:05:55 -0400

    update test cases in grpc_query_test w/ dynamicTxFee

--- x/evm/keeper/grpc_query_test.go
+++ x/evm/keeper/grpc_query_test.go
@@ -485,32 +485,33 @@ func (suite *KeeperTestSuite) TestEstimateGas() {
 		gasCap uint64
 	)
 	testCases := []struct {
-		msg      string
-		malleate func()
-		expPass  bool
-		expGas   uint64
+		msg          string
+		malleate     func()
+		expPass      bool
+		expGas       uint64
+		dynamicTxFee bool
 	}{
 		// should success, because transfer value is zero
 		{"default args", func() {
 			args = types.TransactionArgs{To: &common.Address{}}
-		}, true, 21000},
+		}, true, 21000, false},
 		// should fail, because the default From address(zero address) don't have fund
 		{"not enough balance", func() {
 			args = types.TransactionArgs{To: &common.Address{}, Value: (*hexutil.Big)(big.NewInt(100))}
-		}, false, 0},
+		}, false, 0, false},
 		// should success, enough balance now
 		{"enough balance", func() {
 			args = types.TransactionArgs{To: &common.Address{}, From: &suite.address, Value: (*hexutil.Big)(big.NewInt(100))}
-		}, false, 0},
+		}, false, 0, false},
 		// should success, because gas limit lower than 21000 is ignored
 		{"gas exceed allowance", func() {
 			args = types.TransactionArgs{To: &common.Address{}, Gas: &gasHelper}
-		}, true, 21000},
+		}, true, 21000, false},
 		// should fail, invalid gas cap
 		{"gas exceed global allowance", func() {
 			args = types.TransactionArgs{To: &common.Address{}}
 			gasCap = 20000
-		}, false, 0},
+		}, false, 0, false},
 		// estimate gas of an erc20 contract deployment, the exact gas number is checked with geth
 		{"contract deployment", func() {
 			ctorArgs, err := ContractABI.Pack("", &suite.address, sdk.NewIntWithDecimal(1000, 18).BigInt())
@@ -520,7 +521,7 @@ func (suite *KeeperTestSuite) TestEstimateGas() {
 				From: &suite.address,
 				Data: (*hexutil.Bytes)(&data),
 			}
-		}, true, 1186778},
+		}, true, 1186778, false},
 		// estimate gas of an erc20 transfer, the exact gas number is checked with geth
 		{"erc20 transfer", func() {
 			contractAddr := suite.DeployTestContract(suite.T(), suite.address, sdk.NewIntWithDecimal(1000, 18).BigInt())
@@ -528,11 +529,46 @@ func (suite *KeeperTestSuite) TestEstimateGas() {
 			transferData, err := ContractABI.Pack("transfer", common.HexToAddress("0x378c50D9264C63F3F92B806d4ee56E9D86FfB3Ec"), big.NewInt(1000))
 			suite.Require().NoError(err)
 			args = types.TransactionArgs{To: &contractAddr, From: &suite.address, Data: (*hexutil.Bytes)(&transferData)}
-		}, true, 51880},
+		}, true, 51880, false},
+
+		// repeated tests with dynamicTxFee
+		{"default args w/ dynamicTxFee", func() {
+			args = types.TransactionArgs{To: &common.Address{}}
+		}, true, 21000, true},
+		{"not enough balance w/ dynamicTxFee", func() {
+			args = types.TransactionArgs{To: &common.Address{}, Value: (*hexutil.Big)(big.NewInt(100))}
+		}, false, 0, true},
+		{"enough balance w/ dynamicTxFee", func() {
+			args = types.TransactionArgs{To: &common.Address{}, From: &suite.address, Value: (*hexutil.Big)(big.NewInt(100))}
+		}, false, 0, true},
+		{"gas exceed allowance w/ dynamicTxFee", func() {
+			args = types.TransactionArgs{To: &common.Address{}, Gas: &gasHelper}
+		}, true, 21000, true},
+		{"gas exceed global allowance w/ dynamicTxFee", func() {
+			args = types.TransactionArgs{To: &common.Address{}}
+			gasCap = 20000
+		}, false, 0, true},
+		{"contract deployment w/ dynamicTxFee", func() {
+			ctorArgs, err := ContractABI.Pack("", &suite.address, sdk.NewIntWithDecimal(1000, 18).BigInt())
+			suite.Require().NoError(err)
+			data := append(ContractBin, ctorArgs...)
+			args = types.TransactionArgs{
+				From: &suite.address,
+				Data: (*hexutil.Bytes)(&data),
+			}
+		}, true, 1186778, true},
+		{"erc20 transfer w/ dynamicTxFee", func() {
+			contractAddr := suite.DeployTestContract(suite.T(), suite.address, sdk.NewIntWithDecimal(1000, 18).BigInt())
+			suite.Commit()
+			transferData, err := ContractABI.Pack("transfer", common.HexToAddress("0x378c50D9264C63F3F92B806d4ee56E9D86FfB3Ec"), big.NewInt(1000))
+			suite.Require().NoError(err)
+			args = types.TransactionArgs{To: &contractAddr, From: &suite.address, Data: (*hexutil.Bytes)(&transferData)}
+		}, true, 51880, true},
 	}
 
 	for _, tc := range testCases {
 		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			suite.dynamicTxFee = tc.dynamicTxFee
 			suite.SetupTest()
 			gasCap = 25_000_000
 			tc.malleate()
@@ -553,6 +589,7 @@ func (suite *KeeperTestSuite) TestEstimateGas() {
 			}
 		})
 	}
+	suite.dynamicTxFee = false // reset flag
 }
 
 func (suite *KeeperTestSuite) TestTraceTx() {
@@ -568,6 +605,7 @@ func (suite *KeeperTestSuite) TestTraceTx() {
 		malleate      func()
 		expPass       bool
 		traceResponse []byte
+		dynamicTxFee  bool
 	}{
 		{
 			msg: "default trace",
@@ -586,10 +624,30 @@ func (suite *KeeperTestSuite) TestTraceTx() {
 			expPass:       true,
 			traceResponse: []byte{0x5b, 0x5d},
 		},
+		{
+			msg: "default trace with dynamicTxFee",
+			malleate: func() {
+				traceConfig = nil
+			},
+			expPass:       true,
+			traceResponse: []byte{0x7b, 0x22, 0x67, 0x61, 0x73, 0x22, 0x3a, 0x33, 0x34, 0x38, 0x32, 0x38, 0x2c, 0x22, 0x66, 0x61, 0x69, 0x6c, 0x65, 0x64, 0x22, 0x3a, 0x66, 0x61, 0x6c, 0x73, 0x65, 0x2c, 0x22, 0x72, 0x65, 0x74, 0x75, 0x72, 0x6e, 0x56, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x3a, 0x22, 0x22, 0x2c, 0x22, 0x73, 0x74, 0x72, 0x75, 0x63, 0x74, 0x4c, 0x6f, 0x67, 0x73, 0x22, 0x3a, 0x5b, 0x5d, 0x7d},
+			dynamicTxFee:  true,
+		}, {
+			msg: "javascript tracer with dynamicTxFee",
+			malleate: func() {
+				traceConfig = &types.TraceConfig{
+					Tracer: "{data: [], fault: function(log) {}, step: function(log) { if(log.op.toString() == \"CALL\") this.data.push(log.stack.peek(0)); }, result: function() { return this.data; }}",
+				}
+			},
+			expPass:       true,
+			traceResponse: []byte{0x5b, 0x5d},
+			dynamicTxFee:  true,
+		},
 	}
 
 	for _, tc := range testCases {
 		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
+			suite.dynamicTxFee = tc.dynamicTxFee
 			suite.SetupTest()
 			// Deploy contract
 			contractAddr := suite.DeployTestContract(suite.T(), suite.address, sdk.NewIntWithDecimal(1000, 18).BigInt())
@@ -614,4 +672,6 @@ func (suite *KeeperTestSuite) TestTraceTx() {
 			}
 		})
 	}
+
+	suite.dynamicTxFee = false // reset flag
 }
