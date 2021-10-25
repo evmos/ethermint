a96d53996e859293435c7d5ceaa951fec4920571
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2021-10-25 09:28:08 +0800

    fix: importer test is run on rpc tests check
    restricted "go test" invocation in integration test scripts to e2e and rpc sub-dirs
    closes #694

--- scripts/start.sh
+++ scripts/start.sh
@@ -124,8 +124,9 @@ if [[ -z $TEST || $TEST == "rpc" ]]; then
     for i in $(seq 1 "$TEST_QTD"); do
         HOST_RPC=http://$IP_ADDR:$RPC_PORT"$i"
         echo "going to test ethermint node $HOST_RPC ..."
-        MODE=$MODE HOST=$HOST_RPC go test ./tests/... -timeout=300s -v -short
-        
+        MODE=$MODE HOST=$HOST_RPC go test ./tests/e2e/... -timeout=300s -v -short
+        MODE=$MODE HOST=$HOST_RPC go test ./tests/rpc/... -timeout=300s -v -short
+
         RPC_FAIL=$?
     done
     
