2e6b2b85ea1d292a4a0f9adf07652e1611c29feb
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2021-11-18 16:53:04 +0100

    Fee-Market(Keeper): WIP ERIP1559 tests

--- x/feemarket/keeper/eip1559_test.go
+++ x/feemarket/keeper/eip1559_test.go
@@ -4,24 +4,41 @@ func (suite *KeeperTestSuite) TestCalculateBaseFee() {
 	testCases := []struct {
 		name      string
 		NoBaseFee bool
+		malleate  func()
+		expFee    int64
 	}{
 		{
 			"without BaseFee",
 			true,
+			func() {},
+			0,
 		},
 		{
-			"with BaseFee",
+			"with BaseFee - initial EIP-1559 block",
 			false,
+			func() {},
+			suite.app.FeeMarketKeeper.GetParams(suite.ctx).InitialBaseFee,
 		},
+		// {
+		// 	"with BaseFee - non-initial EIP-1559 block",
+		// 	false,
+		// 	func() {
+
+		// 		suite.app.Commit()
+		// 	},
+		// 	suite.app.FeeMarketKeeper.GetParams(suite.ctx).InitialBaseFee,
+		// },
 	}
 	for _, tc := range testCases {
 		params := suite.app.FeeMarketKeeper.GetParams(suite.ctx)
 		params.NoBaseFee = tc.NoBaseFee
 		suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
 
-		exp := params.InitialBaseFee
+		tc.malleate()
 
 		fee := suite.app.FeeMarketKeeper.CalculateBaseFee(suite.ctx)
+
+		exp := tc.expFee
 		if tc.NoBaseFee {
 			suite.Require().Nil(fee, tc.name)
 		} else {
