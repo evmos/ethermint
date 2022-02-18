44ea3ced53f6e63a7d52f401385636f619619116
HuangYi <huang@crypto.com>
2022-02-18 15:21:39 +0800

    redirect go-ethereum's logs to cosmos logger
    Closes: #862
    
    Map go-ethereum's log levels to cosmos ones:
    trace -> debug
    debug -> debug
    info -> info
    warn -> info
    error -> error
    crit -> error

--- server/json_rpc.go
+++ server/json_rpc.go
@@ -10,6 +10,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/server"
 	"github.com/cosmos/cosmos-sdk/server/types"
+	ethlog "github.com/ethereum/go-ethereum/log"
 	ethrpc "github.com/ethereum/go-ethereum/rpc"
 	"github.com/tharsis/ethermint/rpc"
 
@@ -19,6 +20,25 @@ import (
 // StartJSONRPC starts the JSON-RPC server
 func StartJSONRPC(ctx *server.Context, clientCtx client.Context, tmRPCAddr, tmEndpoint string, config config.Config) (*http.Server, chan struct{}, error) {
 	tmWsClient := ConnectTmWS(tmRPCAddr, tmEndpoint, ctx.Logger)
+
+	logger := ctx.Logger.With("module", "geth")
+	ethlog.Root().SetHandler(ethlog.FuncHandler(func(r *ethlog.Record) error {
+		switch r.Lvl {
+		case ethlog.LvlTrace:
+			fallthrough
+		case ethlog.LvlDebug:
+			logger.Debug(r.Msg, r.Ctx...)
+		case ethlog.LvlInfo:
+			logger.Info(r.Msg, r.Ctx...)
+		case ethlog.LvlWarn:
+			logger.Info(r.Msg, r.Ctx...)
+		case ethlog.LvlError:
+			logger.Error(r.Msg, r.Ctx...)
+		case ethlog.LvlCrit:
+			logger.Error(r.Msg, r.Ctx...)
+		}
+		return nil
+	}))
 
 	rpcServer := ethrpc.NewServer()
 
