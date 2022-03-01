20e806771b0ebb80b43f71a9f10304127456d4e0
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-03-01 06:28:41 -0300

    Update x/evm/keeper/grpc_query.go

--- x/evm/keeper/grpc_query.go
+++ x/evm/keeper/grpc_query.go
@@ -544,10 +544,12 @@ func (k *Keeper) traceTx(
 	// Depending on the tracer type, format and return the trace result data.
 	switch tracer := tracer.(type) {
 	case *logger.StructLogger:
-		returnVal := fmt.Sprintf("%x", res.Return())
+		returnVal := ""
 		revert := res.Revert()
 		if len(revert) > 0 {
 			returnVal = fmt.Sprintf("%x", revert)
+		} else {
+			returnVal = fmt.Sprintf("%x", res.Return())
 		}
 		result = types.ExecutionResult{
 			Gas:         res.GasUsed,
