9041e0a786258d9b05cde8564b029bcfb2c1e1bb
HuangYi <huang@crypto.com>
2022-03-08 10:25:27 +0800

    add flags

--- server/flags/flags.go
+++ server/flags/flags.go
@@ -31,16 +31,18 @@ const (
 
 // JSON-RPC flags
 const (
-	JSONRPCEnable        = "json-rpc.enable"
-	JSONRPCAPI           = "json-rpc.api"
-	JSONRPCAddress       = "json-rpc.address"
-	JSONWsAddress        = "json-rpc.ws-address"
-	JSONRPCGasCap        = "json-rpc.gas-cap"
-	JSONRPCEVMTimeout    = "json-rpc.evm-timeout"
-	JSONRPCTxFeeCap      = "json-rpc.txfee-cap"
-	JSONRPCFilterCap     = "json-rpc.filter-cap"
-	JSONRPCLogsCap       = "json-rpc.logs-cap"
-	JSONRPCBlockRangeCap = "json-rpc.block-range-cap"
+	JSONRPCEnable          = "json-rpc.enable"
+	JSONRPCAPI             = "json-rpc.api"
+	JSONRPCAddress         = "json-rpc.address"
+	JSONWsAddress          = "json-rpc.ws-address"
+	JSONRPCGasCap          = "json-rpc.gas-cap"
+	JSONRPCEVMTimeout      = "json-rpc.evm-timeout"
+	JSONRPCTxFeeCap        = "json-rpc.txfee-cap"
+	JSONRPCFilterCap       = "json-rpc.filter-cap"
+	JSONRPCLogsCap         = "json-rpc.logs-cap"
+	JSONRPCBlockRangeCap   = "json-rpc.block-range-cap"
+	JSONRPCHTTPTimeout     = "json-rpc.http-timeout"
+	JSONRPCHTTPIdleTimeout = "json-rpc.http-idle-timeout"
 )
 
 // EVM flags
