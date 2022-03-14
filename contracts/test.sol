22da945788bfa7f88ef8ed9d722f7ff2b4069ed1
litvinsky <60661362+litvinsky@users.noreply.github.com>
2022-03-14 22:42:34 +0100

    Create test.sol

---
+++ contracts/test.sol
@@ -0,0 +1,15 @@
+
+
+// File: tools/first-contract.sol
+
+pragma solidity >0.4.0 <0.7.0;
+contract SimpleStorage {
+    uint storedData;
+    function set(uint x) public {
+        storedData = x;
+    }
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
+// Test 2
