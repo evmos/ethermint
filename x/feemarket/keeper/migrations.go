d7fab630df2e278f94e8c2c4e4ec7526384bd276
HuangYi <huang@crypto.com>
2022-03-17 11:01:11 +0800

    increase feemarket's consensus version and add migration handler
    Closes: #1001

--- x/feemarket/keeper/migrations.go
+++ x/feemarket/keeper/migrations.go
@@ -1,5 +1,11 @@
 package keeper
 
+import (
+	"math/big"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 // Migrator is a struct for handling in-place store migrations.
 type Migrator struct {
 	keeper Keeper
@@ -10,4 +16,16 @@ func NewMigrator(keeper Keeper) Migrator {
 	return Migrator{
 		keeper: keeper,
 	}
+}
+
+func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+	store := ctx.KVStore(m.keeper.storeKey)
+	baseFeeKeyPrefix := []byte{2}
+	bz := store.Get(baseFeeKeyPrefix)
+	if len(bz) > 0 {
+		baseFee := new(big.Int).SetBytes(bz)
+		m.keeper.SetBaseFee(ctx, baseFee)
+	}
+	store.Delete(baseFeeKeyPrefix)
+	return nil
 }
