54cc144be7b086aaf594d8653167c52a7fde2b60
Thomas Nguy <thomas.nguy@crypto.com>
2022-04-05 20:49:43 +0900

    add lint issue

--- x/evm/keeper/state_transition.go
+++ x/evm/keeper/state_transition.go
@@ -5,6 +5,7 @@ import (
 	"math/big"
 
 	tmtypes "github.com/tendermint/tendermint/types"
+	tmtypes "github.com/tendermint/tendermint/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
