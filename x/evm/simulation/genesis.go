f7de9103a03a9ad4cf7e044ab9ffa89653fcc0de
Adu <foriteration@gmail.com>
2022-04-14 17:36:24 +0800

    use types.DefaultEVMDenom
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- x/evm/simulation/genesis.go
+++ x/evm/simulation/genesis.go
@@ -12,7 +12,7 @@ import (
 
 // RandomizedGenState generates a random GenesisState for nft
 func RandomizedGenState(simState *module.SimulationState) {
-	params := types.NewParams(sdk.DefaultBondDenom, true, true, types.DefaultChainConfig())
+	params := types.NewParams(types.DefaultEVMDenom, true, true, types.DefaultChainConfig())
 	if simState.Rand.Uint32()%2 == 0 {
 		params = types.NewParams(types.DefaultEVMDenom, true, true, types.DefaultChainConfig(), 1344, 1884, 2200, 2929, 3198, 3529)
 	}
