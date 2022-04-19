e58931634a60fb9ce1db39a96069d8c9e976d0ae
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2022-04-19 14:42:41 +0200

    rm unique

--- rpc/apis.go
+++ rpc/apis.go
@@ -153,15 +153,3 @@ func RegisterAPINamespace(ns string, creator APICreator) error {
 	apiCreators[ns] = creator
 	return nil
 }
-
-func unique(intSlice []string) []string {
-	keys := make(map[string]bool)
-	var list []string
-	for _, entry := range intSlice {
-		if _, value := keys[entry]; !value {
-			keys[entry] = true
-			list = append(list, entry)
-		}
-	}
-	return list
-}
