fbc6834056d56cd6c91a7a59976064f2a02bc25e
Adu <foriteration@gmail.com>
2022-04-22 15:27:11 +0800

    add paramschange for enableCreate and enableCall

--- x/evm/simulation/genesis.go
+++ x/evm/simulation/genesis.go
@@ -10,18 +10,46 @@ import (
 	"github.com/tharsis/ethermint/x/evm/types"
 )
 
+const (
+	enableCreateKey = "enable_create"
+	enableCallKey   = "enable_call"
+	extraEIPsKey    = "extra_eips"
+)
+
 // GenExtraEIPs randomly generates specific extra eips or not.
 func genExtraEIPs(r *rand.Rand) []int64 {
 	var extraEIPs []int64
-	if r.Uint32()%2 == 0 {
+	// 50% chance of having extra eips
+	if r.Intn(2) == 0 {
 		extraEIPs = []int64{1344, 1884, 2200, 2929, 3198, 3529}
 	}
 	return extraEIPs
 }
 
+func genEnableCreate(r *rand.Rand) bool {
+	// 80% chance of enabling create contract
+	enableCreate := r.Intn(100) < 80
+	return enableCreate
+}
+
+func genEnableCall(r *rand.Rand) bool {
+	// 80% chance of enabling evm account transfer and calling contract
+	enableCall := r.Intn(100) < 80
+	return enableCall
+}
+
 // RandomizedGenState generates a random GenesisState for nft
 func RandomizedGenState(simState *module.SimulationState) {
-	extraEIPs := genExtraEIPs(simState.Rand)
+	// evm params
+	var (
+		extraEIPs []int64
+	)
+
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, extraEIPsKey, &extraEIPs, simState.Rand,
+		func(r *rand.Rand) { extraEIPs = genExtraEIPs(r) },
+	)
+
 	params := types.NewParams(types.DefaultEVMDenom, true, true, types.DefaultChainConfig(), extraEIPs...)
 	evmGenesis := types.NewGenesisState(params, []types.GenesisAccount{})
 
--- x/evm/simulation/params.go
+++ x/evm/simulation/params.go
@@ -3,12 +3,12 @@ package simulation
 // DONTCOVER
 
 import (
+	"fmt"
 	"math/rand"
 
-	"github.com/cosmos/cosmos-sdk/types/module"
+	amino "github.com/cosmos/cosmos-sdk/codec"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
-	"github.com/tharsis/ethermint/encoding"
 	"github.com/tharsis/ethermint/x/evm/types"
 )
 
@@ -19,12 +19,22 @@ func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
 		simulation.NewSimParamChange(types.ModuleName, string(types.ParamStoreKeyExtraEIPs),
 			func(r *rand.Rand) string {
 				extraEIPs := genExtraEIPs(r)
-				amino := encoding.MakeConfig(module.NewBasicManager()).Amino
+				amino := amino.NewLegacyAmino()
 				bz, err := amino.MarshalJSON(extraEIPs)
 				if err != nil {
 					panic(err)
 				}
 				return string(bz)
+			},
+		),
+		simulation.NewSimParamChange(types.ModuleName, string(types.ParamStoreKeyEnableCreate),
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("%v", genEnableCreate(r))
+			},
+		),
+		simulation.NewSimParamChange(types.ModuleName, string(types.ParamStoreKeyEnableCall),
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("%v", genEnableCall(r))
 			},
 		),
 	}
