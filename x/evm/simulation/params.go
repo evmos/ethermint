cb7992127b3122982e3350945d46193589668fe0
Adu <foriteration@gmail.com>
2022-04-22 10:25:51 +0800

    marshal int slice to json

--- x/evm/simulation/params.go
+++ x/evm/simulation/params.go
@@ -3,25 +3,28 @@ package simulation
 // DONTCOVER
 
 import (
-	"fmt"
 	"math/rand"
 
+	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
+	"github.com/tharsis/ethermint/encoding"
 	"github.com/tharsis/ethermint/x/evm/types"
-)
-
-const (
-	keyExtraEIPs = "ExtraEIPs"
 )
 
 // ParamChanges defines the parameters that can be modified by param change proposals
 // on the simulation.
 func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
 	return []simtypes.ParamChange{
-		simulation.NewSimParamChange(types.ModuleName, keyExtraEIPs,
+		simulation.NewSimParamChange(types.ModuleName, string(types.ParamStoreKeyExtraEIPs),
 			func(r *rand.Rand) string {
-				return fmt.Sprintf("\"%d\"", genExtraEIPs(r))
+				extraEIPs := genExtraEIPs(r)
+				amino := encoding.MakeConfig(module.NewBasicManager()).Amino
+				bz, err := amino.MarshalJSON(extraEIPs)
+				if err != nil {
+					panic(err)
+				}
+				return string(bz)
 			},
 		),
 	}
