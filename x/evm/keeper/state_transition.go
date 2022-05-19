9edc87d1147f3513d99370f41f7547c993e54572
Thomas Nguy <81727899+thomas-nguy@users.noreply.github.com>
2022-05-19 19:11:26 +0900

    fix: append `Log` in tx post processing response  (#1088)
    * fix post processing log
    
    * update changelog

--- x/evm/keeper/state_transition.go
+++ x/evm/keeper/state_transition.go
@@ -271,6 +271,8 @@ func (k *Keeper) ApplyTransaction(ctx sdk.Context, tx *ethtypes.Transaction) (*t
 		} else if commit != nil {
 			// PostTxProcessing is successful, commit the tmpCtx
 			commit()
+			// Since the post processing can alter the log, we need to update the result
+			res.Logs = types.NewLogsFromEth(receipt.Logs)
 			ctx.EventManager().EmitEvents(tmpCtx.EventManager().Events())
 		}
 	}
