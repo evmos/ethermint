9428efd594c9e6cfa5cfbe007c934cf99a5aefaf
Loredana <loredana.cirstea@gmail.com>
2022-05-30 22:14:36 +0300

    Use 0 MinGasPrice for transaction simulations

--- x/feemarket/simulation/genesis.go
+++ x/feemarket/simulation/genesis.go
@@ -12,7 +12,7 @@ import (
 
 // RandomizedGenState generates a random GenesisState for nft
 func RandomizedGenState(simState *module.SimulationState) {
-	params := types.NewParams(simState.Rand.Uint32()%2 == 0, simState.Rand.Uint32(), simState.Rand.Uint32(), simState.Rand.Uint64(), simState.Rand.Int63(), sdk.NewDecFromInt(sdk.NewInt(simState.Rand.Int63())))
+	params := types.NewParams(simState.Rand.Uint32()%2 == 0, simState.Rand.Uint32(), simState.Rand.Uint32(), simState.Rand.Uint64(), simState.Rand.Int63(), sdk.ZeroDec())
 
 	blockGas := simState.Rand.Uint64()
 	feemarketGenesis := types.NewGenesisState(params, blockGas)
