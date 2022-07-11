6221fa1f52f6c21e92fa1e3fac56b17a12669453
HuangYi <huang@crypto.com>
2022-07-11 16:15:14 +0800

    Reuse cosmos-sdk client library to create keyring
    Extracted from https://github.com/evmos/ethermint/pull/1168
    Cleanup cmd code for easier to migration to cosmos-sdk 0.46

--- client/keys/utils.go
+++ client/keys/utils.go
@@ -3,7 +3,6 @@ package keys
 import (
 	"fmt"
 	"io"
-	"path/filepath"
 
 	yaml "gopkg.in/yaml.v2"
 
@@ -15,22 +14,9 @@ import (
 const (
 	OutputFormatText = "text"
 	OutputFormatJSON = "json"
-
-	// defaultKeyDBName is the client's subdirectory where keys are stored.
-	defaultKeyDBName = "keys"
 )
 
 type bechKeyOutFn func(keyInfo cryptokeyring.Info) (cryptokeyring.KeyOutput, error)
-
-// NewLegacyKeyBaseFromDir initializes a legacy keybase at the rootDir directory. Keybase
-// options can be applied when generating this new Keybase.
-func NewLegacyKeyBaseFromDir(rootDir string, opts ...cryptokeyring.KeybaseOption) (cryptokeyring.LegacyKeybase, error) {
-	return getLegacyKeyBaseFromDir(rootDir, opts...)
-}
-
-func getLegacyKeyBaseFromDir(rootDir string, opts ...cryptokeyring.KeybaseOption) (cryptokeyring.LegacyKeybase, error) {
-	return cryptokeyring.NewLegacy(defaultKeyDBName, filepath.Join(rootDir, "keys"), opts...)
-}
 
 func printKeyInfo(w io.Writer, keyInfo cryptokeyring.Info, bechKeyOut bechKeyOutFn, output string) {
 	ko, err := bechKeyOut(keyInfo)
