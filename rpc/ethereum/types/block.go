f18292002906447a56fb8facfed32321636e7996
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-07-13 11:43:34 +0200

    add more nolints

--- rpc/ethereum/types/block.go
+++ rpc/ethereum/types/block.go
@@ -15,7 +15,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
-	grpctypes "github.com/cosmos/cosmos-sdk/types/grpc"
+	grpctypes "github.com/cosmos/cosmos-sdk/types/grpc" // nolint: typecheck
 
 	ethermint "github.com/tharsis/ethermint/types"
 )
