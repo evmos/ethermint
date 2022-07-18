f0e56f4750ae2a1b7ce46806416b3e6b128afbe7
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-07-18 17:53:20 +0200

    tests(json-rpc): wip evm_backend unit test setup

---
+++ rpc/backend/evm_backend_test.go
@@ -0,0 +1,57 @@
+package backend_test
+
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/evmos/ethermint/rpc/backend"
+	"github.com/stretchr/testify/suite"
+)
+
+type BackendTestSuite struct {
+	suite.Suite
+	backend *backend.Backend
+}
+
+func TestBackendTestSuite(t *testing.T) {
+	suite.Run(t, new(BackendTestSuite))
+}
+
+func (suite *BackendTestSuite) SetupTest() {
+	ctx := server.NewDefaultContext()
+	clientCtx := client.Context{
+		Height:  1,
+		ChainID: "ethermint_9000-1",
+	}
+	allowUnprotectedTxs := false
+	suite.backend = backend.NewBackend(ctx, ctx.Logger, clientCtx, allowUnprotectedTxs)
+}
+
+func (suite *BackendTestSuite) TestBlockNumber() {
+	testCases := []struct {
+		mame           string
+		malleate       func()
+		expBlockNumber hexutil.Uint64
+		expPass        bool
+	}{
+		{
+			"test",
+			func() {},
+			hexutil.Uint64(0x00),
+			true,
+		},
+	}
+	for _, tc := range testCases {
+
+		blockNumber, err := suite.backend.BlockNumber()
+
+		if tc.expPass {
+			suite.Require().Nil(err)
+			suite.Require().Equal(tc.expBlockNumber, blockNumber)
+		} else {
+			suite.Require().NotNil(err)
+		}
+	}
+}
