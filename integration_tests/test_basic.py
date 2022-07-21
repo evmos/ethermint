884052b7fdb06bfabbf3a814b4ddbff05a436916
Freddy Caceres <freddy.caceres@crypto.com>
2022-07-20 20:57:34 -0400

    remove unnecessary import

--- integration_tests/test_basic.py
+++ integration_tests/test_basic.py
@@ -1,14 +1,3 @@
-import json
-import time
-from pathlib import Path
-
-import pytest
-import web3
-from eth_bloom import BloomFilter
-from eth_utils import abi, big_endian_to_int
-from hexbytes import HexBytes
-from pystarport import cluster, ports
-
 def test_basic(cluster):
     w3 = cluster.w3
     assert w3.eth.chain_id == 9000
