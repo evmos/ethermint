a5d65abdd3f8d57ea0dfe96a48e6ea077e60ce0e
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-07-22 19:02:11 +0200

    tests(json-rpc):add BlockBloom tests

--- rpc/backend/evm_backend_test.go
+++ rpc/backend/evm_backend_test.go
@@ -1,6 +1,12 @@
 package backend
 
-import "github.com/ethereum/go-ethereum/common/hexutil"
+import (
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	evmtypes "github.com/evmos/ethermint/x/evm/types"
+	"github.com/tendermint/tendermint/abci/types"
+	tmrpctypes "github.com/tendermint/tendermint/rpc/core/types"
+)
 
 func (suite *BackendTestSuite) TestBlockNumber() {
 	testCases := []struct {
@@ -23,6 +29,104 @@ func (suite *BackendTestSuite) TestBlockNumber() {
 		if tc.expPass {
 			suite.Require().Nil(err)
 			suite.Require().Equal(tc.expBlockNumber, blockNumber)
+		} else {
+			suite.Require().NotNil(err)
+		}
+	}
+}
+
+// TODO add Tendermint rpc Client mock
+// func (suite *BackendTestSuite) TestGetTendermintBlockByNumber() {
+// 	testCases := []struct {
+// 		mame        string
+// 		malleate    func()
+// 		blocknumber types.BlockNumber
+// 		expBlock    *coretypes.ResultBlock
+// 		expPass     bool
+// 	}{
+// 		{
+// 			"pass",
+// 			func() {},
+// 			types.BlockNumber(1),
+// 			nil,
+// 			true,
+// 		},
+// 	}
+// 	for _, tc := range testCases {
+// 		block, err := suite.backend.GetTendermintBlockByNumber(tc.blocknumber)
+
+// 		if tc.expPass {
+// 			suite.Require().Nil(err)
+// 			suite.Require().Equal(tc.expBlock, block)
+// 		} else {
+// 			suite.Require().NotNil(err)
+// 		}
+// 	}
+// }
+
+func (suite *BackendTestSuite) TestBlockBloom() {
+	testCases := []struct {
+		mame          string
+		malleate      func()
+		blockRes      *tmrpctypes.ResultBlockResults
+		expBlockBloom ethtypes.Bloom
+		expPass       bool
+	}{
+		{
+			"fail - empty block result",
+			func() {},
+			&tmrpctypes.ResultBlockResults{},
+			ethtypes.Bloom{},
+			false,
+		},
+		{
+			"fail - non block bloom event type",
+			func() {},
+			&tmrpctypes.ResultBlockResults{
+				EndBlockEvents: []types.Event{{Type: evmtypes.EventTypeEthereumTx}},
+			},
+			ethtypes.Bloom{},
+			false,
+		},
+		{
+			"fail - nonblock bloom attribute key",
+			func() {},
+			&tmrpctypes.ResultBlockResults{
+				EndBlockEvents: []types.Event{
+					{
+						Type: evmtypes.EventTypeBlockBloom,
+						Attributes: []types.EventAttribute{
+							{Key: []byte(evmtypes.AttributeKeyEthereumTxHash)},
+						},
+					},
+				},
+			},
+			ethtypes.Bloom{},
+			false,
+		},
+		{
+			"pass - nonblock bloom attribute key",
+			func() {},
+			&tmrpctypes.ResultBlockResults{
+				EndBlockEvents: []types.Event{
+					{
+						Type: evmtypes.EventTypeBlockBloom,
+						Attributes: []types.EventAttribute{
+							{Key: []byte(bAttributeKeyEthereumBloom)},
+						},
+					},
+				},
+			},
+			ethtypes.Bloom{},
+			true,
+		},
+	}
+	for _, tc := range testCases {
+		blockBloom, err := suite.backend.BlockBloom(tc.blockRes)
+
+		if tc.expPass {
+			suite.Require().Nil(err)
+			suite.Require().Equal(tc.expBlockBloom, blockBloom)
 		} else {
 			suite.Require().NotNil(err)
 		}
