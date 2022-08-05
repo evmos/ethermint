57ef02c253938509f068c56c88dd3cba048051a2
HuangYi <huang@crypto.com>
2022-08-05 14:23:33 +0800

    add comment

--- server/flags/flags.go
+++ server/flags/flags.go
@@ -14,7 +14,8 @@ const (
 	Transport      = "transport"
 	TraceStore     = "trace-store"
 	CPUProfile     = "cpu-profile"
-	AppDBBackend   = "app-db-backend"
+	// The type of database for application and snapshots databases
+	AppDBBackend = "app-db-backend"
 )
 
 // GRPC-related flags.
