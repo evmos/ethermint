07581b5d860cc4bd677f15a72d1f42d02250e3cc
faddat <jacobgadikian@gmail.com>
2022-08-24 12:59:43 +0000

    fumpt

--- tests/rpc/rpc_pending_test.go
+++ tests/rpc/rpc_pending_test.go
@@ -1,6 +1,6 @@
 // This is a test utility for Ethermint's Web3 JSON-RPC services.
 //
-// To run these tests please first ensure you have the ethermintd running
+// # To run these tests please first ensure you have the ethermintd running
 //
 // You can configure the desired HOST and MODE as well in integration-test-all.sh
 package rpc
