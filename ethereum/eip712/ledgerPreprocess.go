6dea3120be432a8e0f275150eb19c1032829a94f
Austin Chandra <austinchandra@berkeley.edu>
2022-08-30 17:09:55 -0700

    Fix issue with public key; minor refactors

--- ethereum/eip712/ledgerPreprocess.go
+++ ethereum/eip712/ledgerPreprocess.go
@@ -11,29 +11,29 @@ import (
 	"github.com/evmos/ethermint/types"
 )
 
-// ReformatLedgerTx reformats Ledger-signed Cosmos transactions to match the fork expected by Evmos
-// by including the signature in a Web3Tx extension.
-func ReformatLedgerTx(chainID string, keyType cosmoskr.KeyType, txBuilder client.TxBuilder) error {
+// PreprocessLedgerTx reformats Ledger-signed Cosmos transactions to match the fork expected by Ethermint
+// by including the signature in a Web3Tx extension and sending a blank signature in the body.
+func PreprocessLedgerTx(chainID string, keyType cosmoskr.KeyType, txBuilder client.TxBuilder) error {
+	// Only process Ledger transactions
 	if keyType != cosmoskr.TypeLedger {
-		// Only process Ledger transactions
 		return nil
 	}
 
 	// Init extension builder to set Web3 extension
 	extensionBuilder, ok := txBuilder.(authtx.ExtensionOptionsTxBuilder)
 	if !ok {
-		return fmt.Errorf("Error setting extension options: cannot cast to ExtensionOptionsTxBuilder")
+		return fmt.Errorf("cannot cast TxBuilder to ExtensionOptionsTxBuilder")
 	}
 
 	// Get signatures from TxBuilder
 	sigs, err := txBuilder.GetTx().GetSignaturesV2()
 	if err != nil {
-		return fmt.Errorf("Could not get signatures with error: %w", err)
+		return fmt.Errorf("could not get signatures: %w", err)
 	}
 
 	// Verify single-signer
 	if len(sigs) != 1 {
-		return fmt.Errorf("Invalid number of signatures, expected 1 and got %v", len(sigs))
+		return fmt.Errorf("invalid number of signatures, expected 1 and got %v", len(sigs))
 	}
 
 	signature := sigs[0]
@@ -42,19 +42,19 @@ func ReformatLedgerTx(chainID string, keyType cosmoskr.KeyType, txBuilder client
 	// Parse Chain ID as big.Int
 	chainIDInt, err := types.ParseChainID(chainID)
 	if err != nil {
-		return fmt.Errorf("Error parsing chain id: %v\n", err)
+		return fmt.Errorf("could not parse chain id: %v", err)
 	}
 
 	// Add ExtensionOptionsWeb3Tx extension with signature
 	var option *codectypes.Any
 	option, err = codectypes.NewAnyWithValue(&types.ExtensionOptionsWeb3Tx{
-		FeePayer:         signature.PubKey.Address().String(),
+		FeePayer:         txBuilder.GetTx().FeePayer().String(),
 		TypedDataChainID: chainIDInt.Uint64(),
 		FeePayerSig:      sigBytes,
 	})
 	extensionBuilder.SetExtensionOptions(option)
 
-	// Set signature data with to indicate Amino Sign Type
+	// Set blank signature with Amino Sign Type
 	// (Regardless of input signMode, Evmos requires Amino signature type for Ledger)
 	blankSig := signing.SingleSignatureData{
 		SignMode:  signing.SignMode_SIGN_MODE_LEGACY_AMINO_JSON,
@@ -68,7 +68,7 @@ func ReformatLedgerTx(chainID string, keyType cosmoskr.KeyType, txBuilder client
 
 	err = txBuilder.SetSignatures(sig)
 	if err != nil {
-		return fmt.Errorf("Unable to set signatures on payload: %v\n", err)
+		return fmt.Errorf("unable to set signatures on payload: %v", err)
 	}
 
 	return nil
