e1d620da14c6aa4af4b3cbae0ffd8f8fc09e964d
Tuan Pham Anh <baabeetaa@gmail.com>
2022-09-17 05:11:52 +0700

    refactor

--- tests/rpc/ws_test.go
+++ tests/rpc/ws_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/require"
-	"log"
 	"net/url"
 	"os"
 	"strings"
@@ -28,134 +27,148 @@ func init() {
 }
 
 func TestWsSingleRequest(t *testing.T) {
-	log.Printf("test simple rpc request net_version with websocket")
+	t.Log("test simple rpc request net_version with Websocket")
 
 	wc, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
 	require.NoError(t, err)
 	defer wc.Close()
 
-	err = wc.WriteMessage(websocket.TextMessage, []byte(`{"jsonrpc":"2.0","method":"net_version","params":[],"id":1}`))
-	require.NoError(t, err)
+	wsWriteMessage(t, wc, `{"jsonrpc":"2.0","method":"net_version","params":[],"id":1}`)
 
 	time.Sleep(1 * time.Second)
-	_, mb, err := wc.ReadMessage()
-	require.NoError(t, err)
-	log.Printf("recv: %s", mb)
-
-	var msg map[string]interface{}
-	err = json.Unmarshal(mb, &msg)
-	require.NoError(t, err)
+	mb := readMessage(t, wc)
+	msg := jsonUnmarshal(t, mb)
 	result, ok := msg["result"].(string)
 	require.True(t, ok)
 	require.Equal(t, "9000", result)
 }
 
-//func TestBatchRequest(t *testing.T) {
-//	log.Printf("test batch request with websocket")
-//	err := wc.WriteMessage(websocket.TextMessage, []byte(`[{"jsonrpc":"2.0","method":"net_version","params":[],"id":1},{"jsonrpc":"2.0","method":"eth_protocolVersion","params":[],"id":2}]`))
-//	require.NoError(t, err)
-//
-//	time.Sleep(1 * time.Second)
-//	_, mb, err := wc.ReadMessage()
-//	require.NoError(t, err)
-//
-//	log.Printf("recv: %s", mb)
-//}
-
-func TestWsEth_subscribe_newHeads(t *testing.T) {
-	log.Printf("test eth_subscribe newHeads with websocket")
+func TestWsBatchRequest(t *testing.T) {
+	t.Log("test batch request with Websocket")
 
 	wc, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
 	require.NoError(t, err)
 	defer wc.Close()
 
-	err = wc.WriteMessage(websocket.TextMessage, []byte(`{"id":1,"method":"eth_subscribe","params":["newHeads",{}]}`))
-	require.NoError(t, err)
+	wsWriteMessage(t, wc, `[{"jsonrpc":"2.0","method":"net_version","params":[],"id":1},{"jsonrpc":"2.0","method":"eth_protocolVersion","params":[],"id":2}]`)
 
 	time.Sleep(1 * time.Second)
-	_, mb, err := wc.ReadMessage()
-	require.NoError(t, err)
-	log.Printf("recv: %s", mb)
+	mb := readMessage(t, wc)
 
-	var msg map[string]interface{}
+	var msg []map[string]interface{}
 	err = json.Unmarshal(mb, &msg)
 	require.NoError(t, err)
+	require.Equal(t, 2, len(msg))
+
+	// net_version
+	resNetVersion := msg[0]
+	result, ok := resNetVersion["result"].(string)
+	require.True(t, ok)
+	require.Equal(t, "9000", result)
+	id, ok := resNetVersion["id"].(float64)
+	require.True(t, ok)
+	require.Equal(t, 1, int(id))
+
+	// eth_protocolVersion
+	resEthProtocolVersion := msg[1]
+	result, ok = resEthProtocolVersion["result"].(string)
+	require.True(t, ok)
+	require.Equal(t, "0x41", result)
+	id, ok = resEthProtocolVersion["id"].(float64)
+	require.True(t, ok)
+	require.Equal(t, 2, int(id))
+}
+
+func TestWsEth_subscribe_newHeads(t *testing.T) {
+	t.Log("test eth_subscribe newHeads with Websocket")
+
+	wc, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
+	require.NoError(t, err)
+	defer wc.Close()
+
+	wsWriteMessage(t, wc, `{"id":1,"method":"eth_subscribe","params":["newHeads",{}]}`)
+
+	time.Sleep(1 * time.Second)
+	mb := readMessage(t, wc)
+	msg := jsonUnmarshal(t, mb)
 	subscribeId, ok := msg["result"].(string)
 	require.True(t, ok)
 	require.True(t, strings.HasPrefix(subscribeId, "0x"))
 
 	time.Sleep(3 * time.Second)
-	_, mb, err = wc.ReadMessage()
-	require.NoError(t, err)
-	log.Printf("recv: %s", mb)
-	err = json.Unmarshal(mb, &msg)
-	require.NoError(t, err)
+	mb = readMessage(t, wc)
+	msg = jsonUnmarshal(t, mb)
 	method, ok := msg["method"].(string)
 	require.Equal(t, "eth_subscription", method)
 
 	// id should not exist with eth_subscription event
-	_, ok = msg["id"].(uint32)
+	_, ok = msg["id"].(float64)
 	require.False(t, ok)
 
 	wsUnsubscribe(t, wc, subscribeId) // eth_unsubscribe
 }
 
 func TestWsEth_subscribe_log(t *testing.T) {
-	log.Printf("test eth_subscribe log with websocket")
+	t.Log("test eth_subscribe log with websocket")
 
 	wc, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
 	require.NoError(t, err)
 	defer wc.Close()
 
-	strJson := fmt.Sprintf(`{"jsonrpc":"2.0","id":0,"method":"eth_subscribe","params":["logs",{"topics":["%s", "%s"]}]}`, helloTopic, worldTopic)
-	err = wc.WriteMessage(websocket.TextMessage, []byte(strJson))
-	require.NoError(t, err)
+	wsWriteMessage(t, wc, fmt.Sprintf(`{"jsonrpc":"2.0","id":1,"method":"eth_subscribe","params":["logs",{"topics":["%s", "%s"]}]}`, helloTopic, worldTopic))
 
 	time.Sleep(1 * time.Second)
-	_, mb, err := wc.ReadMessage()
-	require.NoError(t, err)
-	log.Printf("recv: %s", mb)
-
-	var msg map[string]interface{}
-	err = json.Unmarshal(mb, &msg)
-	require.NoError(t, err)
+	mb := readMessage(t, wc)
+	msg := jsonUnmarshal(t, mb)
 	subscribeId, ok := msg["result"].(string)
 	require.True(t, ok)
 	require.True(t, strings.HasPrefix(subscribeId, "0x"))
 
 	// do something here to receive subscription messages
 	deployTestContractWithFunction(t)
-	//
 
 	time.Sleep(3 * time.Second)
-	_, mb, err = wc.ReadMessage()
-	require.NoError(t, err)
-	log.Printf("recv: %s", mb)
-	err = json.Unmarshal(mb, &msg)
-	require.NoError(t, err)
+	mb = readMessage(t, wc)
+	msg = jsonUnmarshal(t, mb)
 	method, ok := msg["method"].(string)
 	require.Equal(t, "eth_subscription", method)
 
 	// id should not exist with eth_subscription event
-	_, ok = msg["id"].(uint32)
+	_, ok = msg["id"].(float64)
 	require.False(t, ok)
 
 	wsUnsubscribe(t, wc, subscribeId) // eth_unsubscribe
 }
 
-func wsUnsubscribe(t *testing.T, wc *websocket.Conn, subscribeId string) {
-	log.Printf("eth_unsubscribe %s", subscribeId)
-	err := wc.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(`{"id":1,"method":"eth_unsubscribe","params":["%s"]}`, subscribeId)))
+func wsWriteMessage(t *testing.T, wc *websocket.Conn, jsonStr string) {
+	t.Logf("send: %s", jsonStr)
+	err := wc.WriteMessage(websocket.TextMessage, []byte(jsonStr))
 	require.NoError(t, err)
+}
+
+func wsUnsubscribe(t *testing.T, wc *websocket.Conn, subscribeId string) {
+	t.Logf("eth_unsubscribe %s", subscribeId)
+	wsWriteMessage(t, wc, fmt.Sprintf(`{"id":1,"method":"eth_unsubscribe","params":["%s"]}`, subscribeId))
 
 	time.Sleep(1 * time.Second)
-	_, mb, err := wc.ReadMessage()
-	require.NoError(t, err)
-	log.Printf("recv: %s", mb)
-	var msg map[string]interface{}
-	err = json.Unmarshal(mb, &msg)
-	require.NoError(t, err)
+	mb := readMessage(t, wc)
+	msg := jsonUnmarshal(t, mb)
+
 	result, ok := msg["result"].(bool)
 	require.True(t, ok)
 	require.True(t, result)
+}
+
+func readMessage(t *testing.T, wc *websocket.Conn) []byte {
+	_, mb, err := wc.ReadMessage()
+	require.NoError(t, err)
+	t.Logf("recv: %s", mb)
+	return mb
+}
+
+func jsonUnmarshal(t *testing.T, mb []byte) map[string]interface{} {
+	var msg map[string]interface{}
+	err := json.Unmarshal(mb, &msg)
+	require.NoError(t, err)
+	return msg
 }
