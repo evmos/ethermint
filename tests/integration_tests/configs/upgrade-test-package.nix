37965273dcb55a996eba843f5c338c662fd80365
yihuang <huang@crypto.com>
2022-09-23 08:22:20 +0800

    Update tests/integration_tests/configs/upgrade-test-package.nix

--- tests/integration_tests/configs/upgrade-test-package.nix
+++ tests/integration_tests/configs/upgrade-test-package.nix
@@ -3,6 +3,7 @@ let
   fetchEthermint = rev: builtins.fetchTarball "https://github.com/evmos/ethermint/archive/${rev}.tar.gz";
   released = pkgs.buildGo118Module rec {
     name = "ethermintd";
+    # the commit before https://github.com/evmos/ethermint/pull/943
     src = fetchEthermint "f21592ebfe74da7590eb42ed926dae970b2a9a3f";
     subPackages = [ "cmd/ethermintd" ];
     vendorSha256 = "sha256-ABm5t6R/u2S6pThGrgdsqe8n3fH5tIWw7a57kxJPbYw=";
