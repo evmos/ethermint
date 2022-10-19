436808b158e5c93088fb9d845dd7781c2dc6cd64
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2022-10-19 14:07:54 +0300

    added unit tests for the new param getter methods

--- x/evm/keeper/params_test.go
+++ x/evm/keeper/params_test.go
@@ -5,10 +5,38 @@ import (
 )
 
 func (suite *KeeperTestSuite) TestParams() {
+	// Checks if the default params are set correctly
 	params := suite.app.EvmKeeper.GetParams(suite.ctx)
 	suite.Require().Equal(types.DefaultParams(), params)
+
+	// Check EvmDenom param is set to "inj" and can be retrieved correctly
 	params.EvmDenom = "inj"
 	suite.app.EvmKeeper.SetParams(suite.ctx, params)
-	newParams := suite.app.EvmKeeper.GetParams(suite.ctx)
-	suite.Require().Equal(newParams, params)
+	evmDenom := suite.app.EvmKeeper.GetEVMDenom(suite.ctx)
+	suite.Require().Equal(evmDenom, params.EvmDenom)
+
+	// Check EnableCreate param is set to false and can be retrieved correctly
+	params.EnableCreate = false
+	suite.app.EvmKeeper.SetParams(suite.ctx, params)
+	enableCreate := suite.app.EvmKeeper.GetEnableCreate(suite.ctx)
+	suite.Require().Equal(enableCreate, params.EnableCreate)
+
+	// Check EnableCall param is set to false and can be retrieved correctly
+	params.EnableCall = false
+	suite.app.EvmKeeper.SetParams(suite.ctx, params)
+	enableCall := suite.app.EvmKeeper.GetEnableCall(suite.ctx)
+	suite.Require().Equal(enableCall, params.EnableCall)
+
+	// Check AllowUnprotectedTxs param is set to false and can be retrieved correctly
+	params.AllowUnprotectedTxs = false
+	suite.app.EvmKeeper.SetParams(suite.ctx, params)
+	allowUnprotectedTxs := suite.app.EvmKeeper.GetAllowUnprotectedTxs(suite.ctx)
+	suite.Require().Equal(allowUnprotectedTxs, params.AllowUnprotectedTxs)
+
+	// Check ChainConfig param is set do the DefaultChainConfig and can be retrieved correctly
+	params.ChainConfig = types.DefaultChainConfig()
+	suite.app.EvmKeeper.SetParams(suite.ctx, params)
+	chainConfig := suite.app.EvmKeeper.GetChainConfig(suite.ctx)
+	suite.Require().Equal(chainConfig, params.ChainConfig)
+
 }
--- x/feemarket/keeper/params_test.go
+++ x/feemarket/keeper/params_test.go
@@ -5,10 +5,25 @@ import (
 )
 
 func (suite *KeeperTestSuite) TestSetGetParams() {
+	// Checks if the default params are set correctly
 	params := suite.app.FeeMarketKeeper.GetParams(suite.ctx)
 	suite.Require().Equal(types.DefaultParams(), params)
+
+	// Check ElasticityMultiplier is set to 3 and can be retrieved correctly
 	params.ElasticityMultiplier = 3
 	suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
 	newParams := suite.app.FeeMarketKeeper.GetParams(suite.ctx)
 	suite.Require().Equal(newParams, params)
+
+	// Check BaseFeeEnabled is computed with its default params and can be retrieved correctly
+	suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
+	isBaseFeeEnabled := suite.app.FeeMarketKeeper.GetBaseFeeEnabled(suite.ctx)
+	suite.Require().Equal(isBaseFeeEnabled, true)
+
+	// Check BaseFeeEnabled is computed with alternate params and can be retrieved correctly
+	params.NoBaseFee = true
+	params.EnableHeight = 5
+	suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
+	isBaseFeeEnabled = suite.app.FeeMarketKeeper.GetBaseFeeEnabled(suite.ctx)
+	suite.Require().Equal(isBaseFeeEnabled, false)
 }
