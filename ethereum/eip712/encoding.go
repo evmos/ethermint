cdba1ee43cde40806d1cf29d8bbafe4f6a86475f
Austin Chandra <austinchandra@berkeley.edu>
2022-11-01 11:31:51 -0700

    Update ethereum/eip712/encoding.go
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- ethereum/eip712/encoding.go
+++ ethereum/eip712/encoding.go
@@ -21,6 +21,7 @@ var (
 	ethermintAminoCodec *codec.LegacyAmino
 )
 
+// SetEncodingConfig set the encoding config to the singleton codecs (Amino and Protobuf).
 // The process of unmarshaling SignDoc bytes into a SignDoc object requires having a codec
 // populated with all relevant message types. As a result, we must call this method on app
 // initialization with the app's encoding config.
