beb9e61ec1482acf187613903f715097841d5e88
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-11-03 17:32:47 +0100

    cleanup

--- x/evm/vm/ethermint/journal_entries.go
+++ x/evm/vm/ethermint/journal_entries.go
@@ -1,40 +1,96 @@
 package ethermint
 
 import (
+	"fmt"
+
 	"github.com/ethereum/go-ethereum/common"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	// transferkeeper "github.com/cosmos/ibc-go/v5/modules/apps/transfer/keeper"
+	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 
 	"github.com/evmos/ethermint/x/evm/statedb"
+	"github.com/evmos/ethermint/x/evm/types"
+
+	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
 )
 
 var _ statedb.JournalEntry = ics20TransferChange{}
 
-type balanceCoinsChange struct {
-	account *common.Address
-	prev    sdk.Coins
-}
-
-func (ch balanceCoinsChange) Revert(s *statedb.StateDB) {
-	// FIXME: we don't have the state object exposed
-	// s.getStateObject(*ch.account).setBalance(ch.prev)
-}
-
-func (ch balanceCoinsChange) Dirtied() *common.Address {
-	return ch.account
-}
-
 type ics20TransferChange struct {
-	precompile *ICS20Precompile
-	caller     common.Address
-	sender     *common.Address
-	msg        *ics20Transfer
+	packet     channeltypes.Packet
+	bankKeeper types.BankKeeper
 }
 
-func (tc ics20TransferChange) Revert(*statedb.StateDB) {
-	// FIXME: this needs to be sequential balance changes
+func (tc ics20TransferChange) Revert(stateDB *statedb.StateDB) {
+	var data transfertypes.FungibleTokenPacketData
+
+	if err := types.ModuleCdc.UnmarshalJSON(tc.packet.GetData(), &data); err != nil {
+		err = sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "cannot unmarshal ICS-20 transfer packet data")
+		panic(err)
+	}
+
+	if err := tc.refundPacketToken(stateDB.Context(), tc.packet, data); err != nil {
+		panic(err)
+	}
 }
 
+// Dirtied returns nil since the ethereum account information from the state object
+// has not been modified.
 func (tc ics20TransferChange) Dirtied() *common.Address {
+	return nil
+}
+
+// TODO: use transfer keeper instead
+// See: https://github.com/cosmos/ibc-go/pull/2660
+func (tc ics20TransferChange) refundPacketToken(ctx sdk.Context, packet channeltypes.Packet, data transfertypes.FungibleTokenPacketData) error {
+	// NOTE: packet data type already checked in handler.go
+
+	// parse the denomination from the full denom path
+	trace := transfertypes.ParseDenomTrace(data.Denom)
+
+	// parse the transfer amount
+	transferAmount, ok := sdk.NewIntFromString(data.Amount)
+	if !ok {
+		return sdkerrors.Wrapf(transfertypes.ErrInvalidAmount, "unable to parse transfer amount (%s) into math.Int", data.Amount)
+	}
+	token := sdk.NewCoin(trace.IBCDenom(), transferAmount)
+
+	// decode the sender address
+	sender, err := sdk.AccAddressFromBech32(data.Sender)
+	if err != nil {
+		return err
+	}
+
+	if transfertypes.SenderChainIsSource(packet.GetSourcePort(), packet.GetSourceChannel(), data.Denom) {
+		// unescrow tokens back to sender
+		escrowAddress := transfertypes.GetEscrowAddress(packet.GetSourcePort(), packet.GetSourceChannel())
+		if err := tc.bankKeeper.SendCoins(ctx, escrowAddress, sender, sdk.NewCoins(token)); err != nil {
+			// NOTE: this error is only expected to occur given an unexpected bug or a malicious
+			// counterparty module. The bug may occur in bank or any part of the code that allows
+			// the escrow address to be drained. A malicious counterparty module could drain the
+			// escrow address by allowing more tokens to be sent back then were escrowed.
+			return sdkerrors.Wrap(
+				err,
+				"unable to unescrow tokens, this may be caused by a malicious counterparty module or a bug: please open an issue on counterparty module",
+			)
+		}
+
+		return nil
+	}
+
+	// mint vouchers back to sender
+	if err := tc.bankKeeper.MintCoins(
+		ctx, types.ModuleName, sdk.NewCoins(token),
+	); err != nil {
+		return err
+	}
+
+	if err := tc.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sender, sdk.NewCoins(token)); err != nil {
+		panic(fmt.Sprintf("unable to send coins from module to account despite previously minting coins to module account: %v", err))
+	}
+
 	return nil
 }
--- x/evm/vm/ethermint/precompiles.go
+++ x/evm/vm/ethermint/precompiles.go
@@ -3,12 +3,11 @@ package ethermint
 // TODO: add
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"math/big"
 	"strings"
+	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
@@ -18,17 +17,16 @@ import (
 	evm "github.com/evmos/ethermint/x/evm/vm"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	ibctransferkeeper "github.com/cosmos/ibc-go/v5/modules/apps/transfer/keeper"
-	ibctransfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	transferkeeper "github.com/cosmos/ibc-go/v5/modules/apps/transfer/keeper"
+	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v5/modules/core/02-client/types"
-	ibcchannelkeeper "github.com/cosmos/ibc-go/v5/modules/core/04-channel/keeper"
-	ibcchanneltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
+	channelkeeper "github.com/cosmos/ibc-go/v5/modules/core/04-channel/keeper"
+	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
 )
 
 var (
-	TransferMethod     abi.Method
-	HasCommitMethod    abi.Method
-	QueryNextSeqMethod abi.Method
+	TransferMethod abi.Method
 
 	_ evm.StatefulPrecompiledContract = (*ICS20Precompile)(nil)
 )
@@ -58,7 +56,8 @@ func init() {
 			},
 			{
 				Name: "sender",
-				Type: addressType, // NOTE: sender is always local so we use common.Address instead of the string type
+				// NOTE: sender is always local so we use common.Address instead of the string type
+				Type: addressType,
 			},
 			{
 				Name: "receiver",
@@ -91,14 +90,14 @@ func init() {
 }
 
 type ICS20Precompile struct {
-	channelKeeper  *ibcchannelkeeper.Keeper
-	transferKeeper *ibctransferkeeper.Keeper
+	channelKeeper  *channelkeeper.Keeper
+	transferKeeper *transferkeeper.Keeper
 	bankKeeper     types.BankKeeper
 }
 
 func NewICS20Precompile(
-	channelKeeper *ibcchannelkeeper.Keeper,
-	transferKeeper *ibctransferkeeper.Keeper,
+	channelKeeper *channelkeeper.Keeper,
+	transferKeeper *transferkeeper.Keeper,
 	bankKeeper types.BankKeeper,
 ) evm.StatefulPrecompiledContract {
 	return &ICS20Precompile{
@@ -127,199 +126,172 @@ func (ic *ICS20Precompile) RunStateful(evm evm.EVM, caller common.Address, input
 	ctx := stateDB.Context()
 
 	methodID := string(input[:4])
+	argsBz := input[4:]
+
 	switch methodID {
-
 	case string(TransferMethod.ID):
-
-		args, err := TransferMethod.Inputs.Unpack(input[4:])
-		if err != nil {
-			return nil, errors.New("fail to unpack input arguments")
-		}
-
-		if len(args) != 8 {
-			return nil, fmt.Errorf("invalid input arguments. Expected 8, got %d", len(args))
-		}
-
-		srcDenom := args[2].(string)
-		portID := args[0].(string)
-		channelID := args[1].(string)
-		timeout := args[5].(*big.Int)
-		sender := args[6].(common.Address)
-		receiver := args[7].(string)
-		amount := args[8].(*big.Int)
-
-
-
-		timeoutTimestamp := uint64(ctx.BlockTime().UnixNano()) + timeout.Uint64()
-		timeoutHeight := clienttypes.ZeroHeight()
-
-		// Use instance to prevent errors on denom or 
-		token := sdk.Coin{
-			Denom:  srcDenom,
-			Amount: sdk.NewIntFromBigInt(amount),
-		}
-
-		src := sdk.AccAddress(sender.Bytes())
-
-		transfer := &ibctransfertypes.MsgTransfer{
-			SourcePort:       portID,
-			SourceChannel:    channelID,
-			Token:            token,
-			Sender:           src.String(), // convert to bech32 format
-			Receiver:         receiver,
-			TimeoutHeight:    timeoutHeight,
-			TimeoutTimestamp: timeoutTimestamp,
-		}
-
-		if err := transfer.ValidateBasic(); err != nil {
-			return nil, err
-		}
-
-		// TODO: create balance change journal entries for each ICS20 case
-		// NOTE: this needs to be done via balance diff since we can't
-		// set the balance
-
-		if _, ok := ic.msgs[caller]; !ok {
-			ic.msgs[caller] = make(map[common.Address]*ics20Transfer)
-		}
-
-		msgs := ic.msgs[caller]
-		msg, ok := msgs[sender]
-		if ok {
-			msg.dirtyAmount = new(big.Int).Sub(msg.dirtyAmount, amount)
-		} else {
-			// query original amount
-			addr := sdk.AccAddress(sender.Bytes())
-			originAmount := ic.bankKeeper.GetBalance(ctx, addr, srcDenom).Amount.BigInt()
-			dirtyAmount := new(big.Int).Sub(originAmount, amount)
-			msg = newICS20Transfer(
-				transfer,
-				ratio,
-				originAmount,
-				dirtyAmount,
-			)
-			msgs[sender] = msg
-		}
-
-		stateDB.AppendJournalEntry(ics20TransferChange{ic, caller, &sender, msg})
-		sequence, _ := ic.channelKeeper.GetNextSequenceSend(ctx, portID, channelID)
-
-		status := ic.channelKeeper.HasPacketCommitment(ctx, portID, channelID, sequence)
-
-		fmt.Printf("TransferMethod sequence: %d, %+v\n", sequence, status)
-
-		return TransferMethod.Outputs.Pack(new(big.Int).SetUint64(sequence))
-
+		return ic.Transfer(ctx, argsBz, stateDB)
 	default:
 		return nil, fmt.Errorf("unknown method '%s'", methodID)
 	}
 }
 
-func (ic *ICS20Precompile) Commit(ctx sdk.Context) error {
-	// FIXME: use slices to avoid non-determinism
-	for _, msgs := range ic.msgs {
-		for sender, msg := range msgs {
-			acc, err := sdk.AccAddressFromBech32(msg.msg.Sender)
-			if err != nil {
-				return err
-			}
+func (ic *ICS20Precompile) Transfer(ctx sdk.Context, argsBz []byte, stateDB statedb.ExtStateDB) ([]byte, error) {
+	args, err := TransferMethod.Inputs.Unpack(argsBz)
+	if err != nil {
+		return nil, errors.New("fail to unpack input arguments")
+	}
 
-			c := msg.msg.Token
-			ratio := sdk.NewIntFromBigInt(msg.ratio)
-			amount8decRem := c.Amount.Mod(ratio)
-			amountToBurn := c.Amount.Sub(amount8decRem)
-			if amountToBurn.IsZero() {
-				// Amount too small
-				continue
-			}
+	msg, err := ic.checkArgs(args, ctx.BlockTime())
+	if err != nil {
+		return nil, err
+	}
 
-			changed := msgs[sender].Changed()
-			coins := sdk.NewCoins(sdk.NewCoin(msg.transfer.Token.Denom, amountToBurn))
-			amount8dec := c.Amount.Quo(ratio)
+	packet, err := ic.buildPacket(ctx,
+		msg.SourcePort, msg.SourceChannel,
+		msg.Token,
+		msg.Sender, msg.Receiver,
+		msg.TimeoutHeight, msg.TimeoutTimestamp,
+	)
+	if err != nil {
+		return nil, err
+	}
 
-			hash := sha256.Sum256([]byte(fmt.Sprintf("%s/%s/%s", ibctransfertypes.ModuleName, msg.transfer.SourceChannel, msg.dstDenom)))
-			ibcDenom := fmt.Sprintf("ibc/%s", strings.ToUpper(hex.EncodeToString(hash[:])))
-			ibcCoin := sdk.NewCoin(ibcDenom, amount8dec)
+	cacheCtx, writeFn := ctx.CacheContext()
 
-			switch changed.Sign() {
-			case -1:
-				// Send evm tokens to escrow address
-				if err = ic.bankKeeper.SendCoinsFromAccountToModule(
-					ctx, acc, ic.module, coins); err != nil {
-					return err
-				}
+	if _, err := ic.transferKeeper.Transfer(sdk.WrapSDKContext(cacheCtx), msg); err != nil {
+		return nil, err
+	}
 
-				// Burns the evm tokens
-				if err := ic.bankKeeper.BurnCoins(
-					ctx, ic.module, coins); err != nil {
-					return err
-				}
+	writeFn()
 
-				// Transfer ibc tokens back to the user
-				if err := ic.bankKeeper.SendCoinsFromModuleToAccount(
-					ctx, ic.module, acc, sdk.NewCoins(ibcCoin),
-				); err != nil {
-					return err
-				}
+	// Add state changes to the journal so we can revert in case of error
+	entry := ics20TransferChange{
+		packet:     packet,
+		bankKeeper: ic.bankKeeper,
+	}
 
-				msg.msg.Token = ibcCoin
-				res, err := ic.transferKeeper.Transfer(ctx, msg.transfer)
-				if err != nil {
-					if ibcchanneltypes.ErrPacketTimeout.Is(err) {
-						if err := ic.bankKeeper.MintCoins(
-							ctx, ic.module, coins); err != nil {
-							return err
-						}
+	stateDB.AppendJournalEntry(entry)
 
-						if err := ic.bankKeeper.SendCoinsFromModuleToAccount(
-							ctx, ic.module, acc, coins); err != nil {
-							return err
-						}
+	return TransferMethod.Outputs.Pack(new(big.Int).SetUint64(packet.Sequence))
+}
 
-						return nil
-					}
+func (ic ICS20Precompile) buildPacket(
+	ctx sdk.Context,
+	sourcePort,
+	sourceChannel string,
+	token sdk.Coin,
+	sender string,
+	receiver string,
+	timeoutHeight clienttypes.Height,
+	timeoutTimestamp uint64,
+) (channeltypes.Packet, error) {
+	sequence, found := ic.channelKeeper.GetNextSequenceSend(ctx, sourcePort, sourceChannel)
+	if !found {
+		return channeltypes.Packet{}, sdkerrors.Wrapf(
+			channeltypes.ErrSequenceSendNotFound, "source port %s, source channel",
+			sourcePort, sourceChannel,
+		)
+	}
 
-					fmt.Printf("Transfer res: %+v, %+v\n", res, err)
-					return err
-				}
-			case 1:
-				// msg.transfer.Token = ibcCoin
-				// res, err := ic.transferKeeper.Transfer(goCtx, msg.transfer)
-				if err := ic.bankKeeper.SendCoinsFromAccountToModule(
-					ctx, acc, ic.module, sdk.NewCoins(ibcCoin),
-				); err != nil {
-					return err
-				}
+	// NOTE: denomination and hex hash correctness checked during msg.ValidateBasic
+	fullDenomPath := token.Denom
+	var err error
 
-				if err := ic.bankKeeper.MintCoins(
-					ctx, ic.module, coins); err != nil {
-					return err
-				}
-
-				if err := ic.bankKeeper.SendCoinsFromModuleToAccount(
-					ctx, ic.module, acc, coins); err != nil {
-					return err
-				}
-				return nil
-			}
+	// deconstruct the token denomination into the denomination trace info
+	// to determine if the sender is the source chain
+	if strings.HasPrefix(token.Denom, "ibc/") {
+		fullDenomPath, err = ic.transferKeeper.DenomPathFromHash(ctx, token.Denom)
+		if err != nil {
+			return channeltypes.Packet{}, err
 		}
 	}
-	return nil
-}
 
-type ics20Transfer struct {
-	msg          *ibctransfertypes.MsgTransfer
-	dstDenom     string
-	ratio        *big.Int
-	originAmount *big.Int
-	dirtyAmount  *big.Int
-}
-
-func newICS20Transfer(msg *ibctransfertypes.MsgTransfer, ratio, originAmount, dirtyAmount *big.Int) *ics20Transfer {
-	return &ics20Transfer{
-		msg:          msg,
-		ratio:        ratio,
-		originAmount: originAmount,
-		dirtyAmount:  dirtyAmount,
+	sourceChannelEnd, found := ic.channelKeeper.GetChannel(ctx, sourcePort, sourceChannel)
+	if !found {
+		return channeltypes.Packet{}, sdkerrors.Wrapf(channeltypes.ErrChannelNotFound, "port ID (%s) channel ID (%s)", sourcePort, sourceChannel)
 	}
+
+	destinationPort := sourceChannelEnd.GetCounterparty().GetPortID()
+	destinationChannel := sourceChannelEnd.GetCounterparty().GetChannelID()
+
+	packetData := transfertypes.NewFungibleTokenPacketData(
+		fullDenomPath, token.Amount.String(), sender, receiver,
+	)
+
+	packet := channeltypes.NewPacket(
+		packetData.GetBytes(),
+		sequence,
+		sourcePort,
+		sourceChannel,
+		destinationPort,
+		destinationChannel,
+		timeoutHeight,
+		timeoutTimestamp,
+	)
+
+	return packet, nil
+}
+
+func (ic *ICS20Precompile) checkArgs(args []interface{}, blockTime time.Time) (*transfertypes.MsgTransfer, error) {
+	if len(args) != 8 {
+		return nil, fmt.Errorf("invalid input arguments. Expected 8, got %d", len(args))
+	}
+
+	sourcePort, _ := args[0].(string)
+	sourceChannel, _ := args[1].(string)
+	denom, _ := args[2].(string)
+
+	amount, ok := args[3].(*big.Int)
+	if !ok || amount == nil {
+		amount = big.NewInt(0)
+	}
+
+	sender, _ := args[4].(common.Address)
+	receiver, ok := args[5].(string)
+
+	timeoutHeightRevisionNumber, ok := args[6].(*big.Int)
+	if !ok || timeoutHeightRevisionNumber == nil {
+		timeoutHeightRevisionNumber = big.NewInt(0)
+	}
+
+	timeoutHeightRevisionHeight, ok := args[7].(*big.Int)
+	if !ok || timeoutHeightRevisionHeight == nil {
+		timeoutHeightRevisionHeight = big.NewInt(0)
+	}
+
+	timeoutDuration, ok := args[8].(*big.Int)
+	if !ok || timeoutDuration == nil {
+		timeoutDuration = big.NewInt(0)
+	}
+
+	timeoutTimestamp := uint64(blockTime.UnixNano()) + timeoutDuration.Uint64()
+	timeoutHeight := clienttypes.Height{
+		RevisionNumber: timeoutHeightRevisionNumber.Uint64(),
+		RevisionHeight: timeoutHeightRevisionHeight.Uint64(),
+	}
+
+	// Use instance to prevent errors on denom or
+	token := sdk.Coin{
+		Denom:  denom,
+		Amount: sdk.NewIntFromBigInt(amount),
+	}
+
+	src := sdk.AccAddress(sender.Bytes())
+
+	msg := &transfertypes.MsgTransfer{
+		SourcePort:       sourcePort,
+		SourceChannel:    sourceChannel,
+		Token:            token,
+		Sender:           src.String(), // convert to bech32 format
+		Receiver:         receiver,
+		TimeoutHeight:    timeoutHeight,
+		TimeoutTimestamp: timeoutTimestamp,
+	}
+
+	if err := msg.ValidateBasic(); err != nil {
+		return nil, err
+	}
+
+	return msg, nil
 }
