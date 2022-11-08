9c4a030ad7ab33a9bdbd3b4476f611163af0a8b4
mmsqe <mavis@crypto.com>
2022-11-08 16:21:16 +0800

    fix build test on mac by update to python3.10

--- nix/testenv.nix
+++ nix/testenv.nix
@@ -1,18 +1,38 @@
-{ pkgs }:
-pkgs.poetry2nix.mkPoetryEnv {
+{ poetry2nix, lib, python310 }:
+poetry2nix.mkPoetryEnv {
   projectDir = ../tests/integration_tests;
-  python = pkgs.python39;
-  overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-    eth-bloom = super.eth-bloom.overridePythonAttrs {
-      preConfigure = ''
-        substituteInPlace setup.py --replace \'setuptools-markdown\' ""
-      '';
-    };
-
-    pystarport = super.pystarport.overridePythonAttrs (
-      old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.poetry ];
-      }
-    );
-  });
+  python = python310;
+  overrides = poetry2nix.overrides.withDefaults (lib.composeManyExtensions [
+    (self: super:
+      let
+        buildSystems = {
+          eth-bloom = [ "setuptools" ];
+          pystarport = [ "poetry" ];
+          durations = [ "setuptools" ];
+          multitail2 = [ "setuptools" ];
+          pytest-github-actions-annotate-failures = [ "setuptools" ];
+          flake8-black = [ "setuptools" ];
+          multiaddr = [ "setuptools" ];
+        };
+      in
+      lib.mapAttrs
+        (attr: systems: super.${attr}.overridePythonAttrs
+          (old: {
+            nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: self.${a}) systems;
+          }))
+        buildSystems
+    )
+    (self: super: {
+      eth-bloom = super.eth-bloom.overridePythonAttrs {
+        preConfigure = ''
+          substituteInPlace setup.py --replace \'setuptools-markdown\' ""
+        '';
+      };
+      pyyaml-include = super.pyyaml-include.overridePythonAttrs {
+        preConfigure = ''
+          substituteInPlace setup.py --replace "setup()" "setup(version=\"1.3\")"
+        '';
+      };
+    })
+  ]);
 }
