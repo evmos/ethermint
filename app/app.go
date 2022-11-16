10ffe309130eb2486e03862d1fad2ed9c775411f
Dev Bear <itsdevbear@berachain.com>
2022-11-15 19:39:21 -0500

    fix validate genesis

--- app/app.go
+++ app/app.go
@@ -145,7 +145,7 @@ var (
 	// and genesis verification.
 	ModuleBasics = module.NewBasicManager(
 		auth.AppModuleBasic{},
-		genutil.AppModuleBasic{},
+		genutil.NewAppModuleBasic(genutiltypes.DefaultMessageValidator),
 		bank.AppModuleBasic{},
 		capability.AppModuleBasic{},
 		staking.AppModuleBasic{},
