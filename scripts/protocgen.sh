c8c9b7b2052c6bc570c0602972a916d5d55f5e2b
MalteHerrmann <malteherrmann.mail@web.de>
2022-11-16 11:20:24 +0100

    address reviews - remove unnecessary ignore in buf.yaml and cosmos-sdk download in the protocgen script

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -5,16 +5,11 @@ set -eo pipefail
 # get protoc executions
 go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 
-# get cosmos sdk
-go get github.com/cosmos/cosmos-sdk 2>/dev/null
-
 echo "Generating gogo proto code"
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    echo "File $file"
     if grep go_package $file &>/dev/null; then
-      echo " >> generating"
       buf generate --template proto/buf.gen.gogo.yaml $file
     fi
   done
