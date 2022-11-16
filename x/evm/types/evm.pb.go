0a3faa6e0e5f9fc6345608525c8289967925636e
MalteHerrmann <malteherrmann.mail@web.de>
2022-11-16 11:02:44 +0100

    Commit changed .pb.go files after updating the protogen scripts

--- x/evm/types/evm.pb.go
+++ x/evm/types/evm.pb.go
@@ -6,7 +6,7 @@ package types
 import (
 	fmt "fmt"
 	github_com_cosmos_cosmos_sdk_types "github.com/cosmos/cosmos-sdk/types"
-	_ "github.com/gogo/protobuf/gogoproto"
+	_ "github.com/cosmos/gogoproto/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	io "io"
 	math "math"
