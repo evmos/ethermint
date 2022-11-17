9e5f4aaa73fce200eb5b0808a978db1f032c3ca4
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2022-11-17 02:35:15 +0100

    tests: Add unit tests for rpc client endpoints (#1409)
    * test: add preliminary unit tests and additional mocks for chain_info, account_info and filters
    
    * tests: added additional mocked client calls
    
    * tests: bumped coverage of call_tx to 56% and chain_info to 77%
    
    * tests: bumped call_tx coverage to 70.2% and added additional mock client calls
    
    * tests: tx_info preliminary tests added for debugging.
    
    * tests: added test coverage for sign_tx and additional mocks
    
    * tests: tx_info test coverage bumped to 60.3%
    
    * test: coverage for tracing_tests now at 72%
    
    * tests: added fee makert query client mocks and bumped chain_info to 87.6% coverage.
    
    * tests: failing Cosmos auth module account query.
    
    * tests: added FeeMarket Params mock to call_tx_test
    
    * cleanup some unused code
    
    * tests: added helper function to test suite for signing a Tx and bumped coverage of tx_info to 71.2%
    
    * test: commented GetAccount error case and bumped chain_info to 90.3% coverage
    
    * test: cleanup of tests in node_info, sign_tx and account_info
    
    * Clean up print
    
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * fix import issues
    
    Co-authored-by: Vladislav Varadinov <vlad@evmos.org>
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
    Co-authored-by: Freddy Caceres <facs95@gmail.com>

--- rpc/backend/backend_suite_test.go
+++ rpc/backend/backend_suite_test.go
@@ -3,6 +3,7 @@ package backend
 import (
 	"bufio"
 	"fmt"
+	"github.com/evmos/ethermint/crypto/ethsecp256k1"
 	"math/big"
 	"os"
 	"path/filepath"
@@ -33,6 +34,7 @@ type BackendTestSuite struct {
 	suite.Suite
 	backend *Backend
 	acc     sdk.AccAddress
+	signer  keyring.Signer
 }
 
 func TestBackendTestSuite(t *testing.T) {
@@ -61,6 +63,10 @@ func (suite *BackendTestSuite) SetupTest() {
 		Seq:     uint64(1),
 	}
 
+	priv, err := ethsecp256k1.GenerateKey()
+	suite.signer = tests.NewSigner(priv)
+	suite.Require().NoError(err)
+
 	encodingConfig := encoding.MakeConfig(app.ModuleBasics)
 	clientCtx := client.Context{}.WithChainID("ethermint_9000-1").
 		WithHeight(1).
@@ -70,13 +76,18 @@ func (suite *BackendTestSuite) SetupTest() {
 		WithAccountRetriever(client.TestAccountRetriever{Accounts: accounts})
 
 	allowUnprotectedTxs := false
-
 	idxer := indexer.NewKVIndexer(dbm.NewMemDB(), ctx.Logger, clientCtx)
 
 	suite.backend = NewBackend(ctx, ctx.Logger, clientCtx, allowUnprotectedTxs, idxer)
 	suite.backend.queryClient.QueryClient = mocks.NewEVMQueryClient(suite.T())
 	suite.backend.clientCtx.Client = mocks.NewClient(suite.T())
+	suite.backend.queryClient.FeeMarket = mocks.NewFeeMarketQueryClient(suite.T())
 	suite.backend.ctx = rpctypes.ContextWithHeight(1)
+
+	// Add codec
+	encCfg := encoding.MakeConfig(app.ModuleBasics)
+	suite.backend.clientCtx.Codec = encCfg.Codec
+
 }
 
 // buildEthereumTx returns an example legacy Ethereum transaction
@@ -157,4 +168,26 @@ func (suite *BackendTestSuite) generateTestKeyring(clientDir string) (keyring.Ke
 	buf := bufio.NewReader(os.Stdin)
 	encCfg := encoding.MakeConfig(app.ModuleBasics)
 	return keyring.New(sdk.KeyringServiceName(), keyring.BackendTest, clientDir, buf, encCfg.Codec, []keyring.Option{hd.EthSecp256k1Option()}...)
+}
+
+func (suite *BackendTestSuite) signAndEncodeEthTx(msgEthereumTx *evmtypes.MsgEthereumTx) []byte {
+	from, priv := tests.NewAddrKey()
+	signer := tests.NewSigner(priv)
+
+	queryClient := suite.backend.queryClient.QueryClient.(*mocks.EVMQueryClient)
+	RegisterParamsWithoutHeader(queryClient, 1)
+
+	ethSigner := ethtypes.LatestSigner(suite.backend.ChainConfig())
+	msgEthereumTx.From = from.String()
+	err := msgEthereumTx.Sign(ethSigner, signer)
+	suite.Require().NoError(err)
+
+	tx, err := msgEthereumTx.BuildTx(suite.backend.clientCtx.TxConfig.NewTxBuilder(), "aphoton")
+	suite.Require().NoError(err)
+
+	txEncoder := suite.backend.clientCtx.TxConfig.TxEncoder()
+	txBz, err := txEncoder(tx)
+	suite.Require().NoError(err)
+
+	return txBz
 }
