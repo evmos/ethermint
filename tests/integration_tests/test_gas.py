5903589a5c6e619a925a50835a03edd4decd07ef
adisaran64 <adi@evmos.org>
2022-11-18 12:49:37 -0800

    add stateful contract calls

--- tests/integration_tests/test_gas.py
+++ tests/integration_tests/test_gas.py
@@ -7,27 +7,27 @@ def test_gas_eth_tx(geth, ethermint):
     # send a transaction with geth
     geth_gas_price = geth.w3.eth.gas_price
     tx = {"to": ADDRS["community"], "value": tx_value, "gasPrice": geth_gas_price}
-    geth_reciept = send_transaction(geth.w3, tx, KEYS["validator"])
+    geth_receipt = send_transaction(geth.w3, tx, KEYS["validator"])
 
     # send an equivalent transaction with ethermint
     ethermint_gas_price = ethermint.w3.eth.gas_price
     tx = {"to": ADDRS["community"], "value": tx_value, "gasPrice": ethermint_gas_price}
-    ethermint_reciept = send_transaction(ethermint.w3, tx, KEYS["validator"])
+    ethermint_receipt = send_transaction(ethermint.w3, tx, KEYS["validator"])
 
     # ensure that the gasUsed is equivalent
-    assert geth_reciept.gasUsed == ethermint_reciept.gasUsed
+    assert geth_receipt.gasUsed == ethermint_receipt.gasUsed
 
 
 def test_gas_deployment(geth, ethermint):
     # deploy an identical contract on geth and ethermint
     # ensure that the gasUsed is equivalent
-    _, geth_contract_reciept = deploy_contract(
+    _, geth_contract_receipt = deploy_contract(
         geth.w3,
         CONTRACTS["TestERC20A"])
-    _, ethermint_contract_reciept = deploy_contract(
+    _, ethermint_contract_receipt = deploy_contract(
         ethermint.w3,
         CONTRACTS["TestERC20A"])
-    assert geth_contract_reciept.gasUsed == ethermint_contract_reciept.gasUsed
+    assert geth_contract_receipt.gasUsed == ethermint_contract_receipt.gasUsed
 
 
 def test_gas_call(geth, ethermint):
@@ -42,9 +42,21 @@ def test_gas_call(geth, ethermint):
         ethermint.w3,
         CONTRACTS["BurnGas"])
 
-    # call the contract locally (eth_call) and compare gas estimates
-    geth_estimated_gas = (geth_contract.functions
-                          .burnGas(function_input).estimate_gas())
-    ethermint_estimated_gas = (ethermint_contract.functions
-                               .burnGas(function_input).estimate_gas())
-    assert geth_estimated_gas == ethermint_estimated_gas
+    # call the contract and get tx receipt for geth
+    geth_gas_price = geth.w3.eth.gas_price
+    geth_txhash = (geth_contract.functions
+                   .burnGas(function_input)
+                   .transact({'from': ADDRS["validator"], "gasPrice": geth_gas_price}))
+    geth_call_receipt = geth.w3.eth.wait_for_transaction_receipt(geth_txhash)
+
+    # repeat the above for ethermint
+    ethermint_gas_price = ethermint.w3.eth.gas_price
+    ethermint_txhash = (ethermint_contract.functions
+                        .burnGas(function_input)
+                        .transact({'from': ADDRS["validator"],
+                                   "gasPrice": ethermint_gas_price}))
+    ethermint_call_receipt = (ethermint.w3.
+                              eth.wait_for_transaction_receipt(ethermint_txhash))
+
+    # ensure that the gasUsed is equivalent
+    assert geth_call_receipt.gasUsed == ethermint_call_receipt.gasUsed
