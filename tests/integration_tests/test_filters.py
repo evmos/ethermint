39414eb2d71bc721bff738d7317a0421218a1fbe
Tom <tomasguerraalda@hotmail.com>
2022-11-21 11:28:29 -0300

    tests(filters) add getLogs test cases

--- tests/integration_tests/test_filters.py
+++ tests/integration_tests/test_filters.py
@@ -1,5 +1,6 @@
 import pytest
 from web3 import Web3
+from eth_abi import abi
 
 from .utils import (
     ADDRS,
@@ -109,16 +110,136 @@ def test_event_log_filter_by_address(cluster):
 def test_get_logs(cluster):
     w3: Web3 = cluster.w3
 
+    # deploy greeter contract
     contract, _ = deploy_contract(w3, CONTRACTS["Greeter"])
 
-    # without tx
+    # calculate topic from event signature
+    topic = w3.keccak(text="ChangeGreeting(address,string)")
+
+    # another topic not related to the contract deployed
+    another_topic = w3.keccak(text="Transfer(address,address,uint256)")
+
+    # without tx - logs should be empty
     assert w3.eth.get_logs({"address": contract.address}) == []
     assert w3.eth.get_logs({"address": ADDRS["validator"]}) == []
 
     # with tx
-    tx = contract.functions.setGreeting("world").build_transaction()
+    # update greeting
+    new_greeting = "hello, world"
+    tx = contract.functions.setGreeting(new_greeting).build_transaction()
     receipt = send_transaction(w3, tx)
     assert receipt.status == 1
 
-    assert len(w3.eth.get_logs({"address": contract.address})) == 1
-    assert len(w3.eth.get_logs({"address": ADDRS["validator"]})) == 0
+    tx_block_num = w3.eth.block_number
+
+    test_cases = [
+        {
+            "name": "get logs by block range - tx block number is within the range",
+            "logs": w3.eth.get_logs({"fromBlock": 1, "toBlock": tx_block_num}),
+            "exp_log": True,
+            "exp_len": None,        # there are other events not belonging to the contract within the block range specified
+        },
+        {
+            "name": "get logs by block range - tx block number outside the range",
+            "logs": w3.eth.get_logs({"fromBlock": 1, "toBlock": 2}),
+            "exp_log": False,
+            "exp_len": 0,
+        },
+        {
+            "name": "get logs by contract address",
+            "logs": w3.eth.get_logs({"address": contract.address}),
+            "exp_log": True,
+            "exp_len": 1,
+        },
+        {
+            "name": "get logs by topic",
+            "logs": w3.eth.get_logs({"topics": [topic.hex()]}),
+            "exp_log": True,
+            "exp_len": 1,
+        },        
+        {
+            "name": "get logs by incorrect topic - should not have logs",
+            "logs": w3.eth.get_logs(
+                {"topics": [another_topic.hex()]}
+            ),
+            "exp_log": False,
+            "exp_len": 0,
+        },
+        {
+            "name": "get logs by multiple topics ('with all' condition)",
+            "logs": w3.eth.get_logs(
+                {
+                    "topics": [
+                        topic.hex(),
+                        another_topic.hex(),
+                    ]
+                }
+            ),
+            "exp_log": False,
+            "exp_len": 0,
+        },
+        {
+            "name": "get logs by multiple topics ('match any' condition)",
+            "logs": w3.eth.get_logs(
+                {
+                    "topics": [
+                        [topic.hex(), another_topic.hex()]
+                    ]
+                }
+            ),
+            "exp_log": True,
+            "exp_len": 1,
+        },
+    ]
+
+    for tc in test_cases:
+        print("Case: {}".format(tc["name"]))
+
+        # logs for validator address should remain empty
+        assert len(w3.eth.get_logs({"address": ADDRS["validator"]})) == 0
+
+        logs = tc["logs"]
+
+        if tc["exp_len"] is not None:
+            assert len(logs) == tc["exp_len"]
+
+        if tc["exp_log"]:
+            found_log = False
+
+            for log in logs:
+                if log["address"] == contract.address:
+                    # this event was emitted only once
+                    # so one log from this contract should exist
+                    # we check the flag to know it is not repeated
+                    assert found_log == False
+
+                    found_log = True
+          
+                    # should have only one topic
+                    assert len(log["topics"]) == 1
+                    assert log["topics"][0] == topic
+
+                    block_hash = log["blockHash"]
+                    # check if the returned block hash is correct
+                    # getBlockByHash
+                    block = w3.eth.get_block(block_hash)
+                    # block should exist
+                    assert block.hash == block_hash
+                    assert block.number == tx_block_num
+
+                    # check tx hash is correct
+                    tx_data = w3.eth.get_transaction(log["transactionHash"])
+                    assert tx_data["blockHash"] == block.hash
+
+                    # check event log data ('from' and 'value' fields)
+                    types = ["address", "string"]
+                    names = ["from", "value"]
+                    values = abi.decode_abi(types, log["data"])
+                    log_data = dict(zip(names, values))
+
+                    # the address stored in the data field may defer on lower/upper case characters
+                    # then, set all as uppercase for assertion
+                    assert log_data["from"].upper() == ADDRS["validator"].upper()
+                    assert log_data["value"] == new_greeting
+                
+            assert found_log == True
