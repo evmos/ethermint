ea44a13d4b9d142d6e494b0b6f5cc08ab214b78e
Dev Bear <itsdevbear@berachain.com>
2022-11-21 21:28:04 -0500

    fix tests

--- app/app_test.go
+++ app/app_test.go
@@ -10,8 +10,6 @@ import (
 
 	"github.com/tendermint/tendermint/libs/log"
 	dbm "github.com/tendermint/tm-db"
-
-	"github.com/evmos/ethermint/encoding"
 )
 
 func TestEthermintAppExport(t *testing.T) {
@@ -20,7 +18,7 @@ func TestEthermintAppExport(t *testing.T) {
 	app.Commit()
 
 	// Making a new app object with the db, so that initchain hasn't been called
-	app2 := NewEthermintApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, DefaultNodeHome, 0, encoding.MakeConfig(ModuleBasics), simapputil.EmptyAppOptions{})
+	app2 := NewEthermintApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, true, map[int64]bool{}, DefaultNodeHome, 0, simapputil.EmptyAppOptions{})
 	_, err := app2.ExportAppStateAndValidators(false, []string{}, []string{})
 	require.NoError(t, err, "ExportAppStateAndValidators should not have an error")
 }
