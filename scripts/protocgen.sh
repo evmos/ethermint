3ad16e1041340949e9ea2557f13c32dcc89c5709
MalteHerrmann <malteherrmann.mail@web.de>
2022-11-24 16:48:34 +0100

    remove unnecessary go get command in protocgen.sh when using cosmos docker image

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+# --------------
+# Commands to run locally
+# docker run --network host --rm -v $(CURDIR):/workspace --workdir /workspace tendermintdev/sdk-proto-gen:v0.7 sh ./protocgen.sh
+#
 set -eo pipefail
-
-# get protoc executions
-go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 
 echo "Generating gogo proto code"
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep go_package $file &>/dev/null; then
-      buf generate --template proto/buf.gen.gogo.yaml $file
+  proto_files=$(find "${dir}" -maxdepth 1 -name '*.proto')
+  for file in $proto_files; do
+    # Check if the go_package in the file is pointing to evmos
+    if grep -q "option go_package.*ethermint" "$file"; then
+      buf generate --template proto/buf.gen.gogo.yaml "$file"
     fi
   done
 done
