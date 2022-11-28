54b9bbaff04c1dab9d26a7d2ec6cbe0fe1ad6f0d
adisaran64 <adi@evmos.org>
2022-11-27 22:56:31 -0800

    add back gas call test

--- tests/integration_tests/test_gas.py
+++ tests/integration_tests/test_gas.py
@@ -31,6 +31,36 @@ def test_gas_deployment(geth, ethermint):
         CONTRACTS["TestERC20A"])
     assert geth_contract_receipt.gasUsed == ethermint_contract_receipt.gasUsed
 
+def test_gas_call(geth, ethermint):
+    function_input = 10
+
+    # deploy an identical contract on geth and ethermint
+    # ensure that the contract has a function which consumes non-trivial gas
+    geth_contract, _ = deploy_contract(
+        geth.w3,
+        CONTRACTS["BurnGas"])
+    ethermint_contract, _ = deploy_contract(
+        ethermint.w3,
+        CONTRACTS["BurnGas"])
+
+    # call the contract and get tx receipt for geth
+    geth_gas_price = geth.w3.eth.gas_price
+    geth_txhash = (geth_contract.functions
+                   .burnGas(function_input)
+                   .transact({'from': ADDRS["validator"], "gasPrice": geth_gas_price}))
+    geth_call_receipt = geth.w3.eth.wait_for_transaction_receipt(geth_txhash)
+
+    # repeat the above for ethermint
+    ethermint_gas_price = ethermint.w3.eth.gas_price
+    ethermint_txhash = (ethermint_contract.functions
+                        .burnGas(function_input)
+                        .transact({'from': ADDRS["validator"],
+                                   "gasPrice": ethermint_gas_price}))
+    ethermint_call_receipt = (ethermint.w3.
+                              eth.wait_for_transaction_receipt(ethermint_txhash))
+
+    # ensure that the gasUsed is equivalent
+    assert geth_call_receipt.gasUsed == ethermint_call_receipt.gasUsed
 
 def test_block_gas_limit(ethermint):
     tx_value = 10
