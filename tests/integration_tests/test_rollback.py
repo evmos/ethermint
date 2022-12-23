29caa1916da93b04fff694bf2bb4150229d33464
mmsqe <tqd0800210105@gmail.com>
2022-12-23 20:58:26 +0800

    tests: make integration test more stable (#1488)
    * wait new blk right before send tx
    
    * larger timeout_commit for priority test
    
    * larger timeout_commit for mempool related test
    
    * mv chain id test to cluster used test
    
    * keep cluster in module scope
    
    * sync gomod2nix
    
    * adjust timeout_commit
    
    * rm prune all in indexer config
    
    * add missing min_gas_multiplier
    
    * wait 1 more blk in upgrade
    
    * only keep 2 validators
    
    * add retry for grpc_eth_call
    
    * wait 1 block before stop
    
    * fix lint
    
    * disable recheck
    
    * bump up upgrade
    
    * sync gomod2nix
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * append node log
    
    * fix lint
    
    * expect less gas after ecd76396eb55dc44535842018c5f13f234af7da3
    
    * allow retry continue on empty rsp
    
    * update gomod2nix
    
    * fix flake
    
    * mod tidy
    
    * keep grpc only test
    
    * tests(integration): enable recheck tx mode
    
    * update gomod2nix
    
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
    Co-authored-by: Freddy Caceres <facs95@gmail.com>
    Co-authored-by: Tom <tomasguerraalda@hotmail.com>

--- tests/integration_tests/test_rollback.py
+++ tests/integration_tests/test_rollback.py
@@ -10,7 +10,7 @@ from .network import setup_custom_ethermint
 from .utils import supervisorctl, wait_for_block, wait_for_port
 
 
-def update_node2_cmd(path, cmd, i):
+def update_node_cmd(path, cmd, i):
     ini_path = path / SUPERVISOR_CONFIG_FILE
     ini = configparser.RawConfigParser()
     ini.read(ini_path)
@@ -29,7 +29,7 @@ def update_node2_cmd(path, cmd, i):
 def post_init(broken_binary):
     def inner(path, base_port, config):
         chain_id = "ethermint_9000-1"
-        update_node2_cmd(path / chain_id, broken_binary, 2)
+        update_node_cmd(path / chain_id, broken_binary, 1)
 
     return inner
 
@@ -66,33 +66,33 @@ def test_rollback(custom_ethermint):
     - use rollback command to rollback the db.
     - switch to correct binary should make the node syncing again.
     """
-    wait_for_port(ports.rpc_port(custom_ethermint.base_port(2)))
+    target_port = ports.rpc_port(custom_ethermint.base_port(1))
+    wait_for_port(target_port)
 
-    print("wait for node2 to sync the first 10 blocks")
-    cli2 = custom_ethermint.cosmos_cli(2)
-    wait_for_block(cli2, 10)
+    print("wait for node1 to sync the first 10 blocks")
+    cli1 = custom_ethermint.cosmos_cli(1)
+    wait_for_block(cli1, 10)
 
     print("wait for a few more blocks on the healthy nodes")
-    cli = custom_ethermint.cosmos_cli(0)
-    wait_for_block(cli, 13)
+    cli0 = custom_ethermint.cosmos_cli(0)
+    wait_for_block(cli0, 13)
 
     # (app hash mismatch happens after the 10th block, detected in the 11th block)
-    print("check node2 get stuck at block 10")
-    assert cli2.block_height() == 10
+    print("check node1 get stuck at block 10")
+    assert cli1.block_height() == 10
 
-    print("stop node2")
+    print("stop node1")
     supervisorctl(
-        custom_ethermint.base_dir / "../tasks.ini", "stop", "ethermint_9000-1-node2"
+        custom_ethermint.base_dir / "../tasks.ini", "stop", "ethermint_9000-1-node1"
     )
 
-    print("do rollback on node2")
-    cli2.rollback()
+    print("do rollback on node1")
+    cli1.rollback()
 
     print("switch to normal binary")
-    update_node2_cmd(custom_ethermint.base_dir, "ethermintd", 2)
+    update_node_cmd(custom_ethermint.base_dir, "ethermintd", 1)
     supervisorctl(custom_ethermint.base_dir / "../tasks.ini", "update")
-    wait_for_port(ports.rpc_port(custom_ethermint.base_port(2)))
+    wait_for_port(target_port)
 
-    print("check node2 sync again")
-    cli2 = custom_ethermint.cosmos_cli(2)
-    wait_for_block(cli2, 15)
+    print("check node1 sync again")
+    wait_for_block(cli1, 15)
