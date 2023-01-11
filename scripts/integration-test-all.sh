66f2822468e03bb28f72e5501f9b9795516dd1b3
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2023-01-11 15:05:39 +0200

    fix: double quote to prevent globbing and word splitting

--- scripts/integration-test-all.sh
+++ scripts/integration-test-all.sh
@@ -3,7 +3,7 @@
 # "stable" mode tests assume data is static
 # "live" mode tests assume data dynamic
 
-SCRIPT=$(basename ${BASH_SOURCE[0]})
+SCRIPT=$(basename "${BASH_SOURCE[0]}")
 TEST=""
 QTD=1
 SLEEP_TIMEOUT=5
