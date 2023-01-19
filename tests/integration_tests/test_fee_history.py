e06236111e14bdf08dfffffcf6f7fe2a604967fd
mmsqe <mavis@crypto.com>
2023-01-19 23:12:09 +0800

    add cross check

--- tests/integration_tests/test_fee_history.py
+++ tests/integration_tests/test_fee_history.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 import pytest
 from web3 import Web3
 
@@ -11,9 +13,7 @@ def custom_ethermint(tmp_path_factory):
     yield from setup_ethermint(path, 26500, long_timeout_commit=True)
 
 
-@pytest.fixture(
-    scope="module", params=["ethermint", "geth", "ethermint-ws"]
-)
+@pytest.fixture(scope="module", params=["ethermint", "geth"])
 def cluster(request, custom_ethermint, geth):
     """
     run on both ethermint and geth
@@ -23,21 +23,39 @@ def cluster(request, custom_ethermint, geth):
         yield custom_ethermint
     elif provider == "geth":
         yield geth
-    elif provider == "ethermint-ws":
-        ethermint_ws = custom_ethermint.copy()
-        ethermint_ws.use_websocket()
-        yield ethermint_ws
     else:
         raise NotImplementedError
 
 
-def test_basic(cluster):
+def test_basic_fee(cluster):
     w3: Web3 = cluster.w3
-    eth_rpc = w3.provider
+    call = w3.provider.make_request
     tx_value = 10
     gas_price = w3.eth.gas_price
     tx = {"to": ADDRS["community"], "value": tx_value, "gasPrice": gas_price}
     send_transaction(w3, tx)
-    for provider in [eth_rpc]:
-        fee_history = provider.make_request("eth_feeHistory", [4, "latest", [100]])
-        assert len(fee_history["result"]["baseFeePerGas"]) == 5
+    range = 4
+    method = "eth_feeHistory"
+    field = "baseFeePerGas"
+    percentiles = [100]
+    height = w3.eth.block_number
+    latest = dict(
+        blocks=["latest", hex(height)],
+        # range of base fee + next fee
+        expect=range + 1,
+    )
+    earliest = dict(
+        blocks=["earliest", "0x0"],
+        # only 1 base fee + next fee
+        expect=2,
+    )
+    for tc in [latest, earliest]:
+        res = []
+        with ThreadPoolExecutor(len(tc["blocks"])) as exec:
+            tasks = [
+                exec.submit(call, method, [range, b, percentiles]) for b in tc["blocks"]
+            ]
+            res = [future.result()["result"][field] for future in as_completed(tasks)]
+        assert len(res) == len(tc["blocks"])
+        assert res[0] == res[1]
+        assert len(res[0]) == tc["expect"]
