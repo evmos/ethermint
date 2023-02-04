4337fb46a3fe0385981b7050a0035c46d79fa42e
mmsqe <mavis@crypto.com>
2023-02-04 22:13:15 +0800

    test delegatecall

--- tests/integration_tests/test_precompiles.py
+++ tests/integration_tests/test_precompiles.py
@@ -1,35 +1,62 @@
-import pytest
-
 from .utils import ADDRS, CONTRACTS, deploy_contract, eth_to_bech32, send_transaction
 
 
-@pytest.mark.parametrize("suffix", [""])
-def test_precompiles(ethermint, suffix):
+def test_call(ethermint):
     w3 = ethermint.w3
     addr = ADDRS["validator"]
     amount = 100
     contract, _ = deploy_contract(w3, CONTRACTS["TestBank"])
-    data = {"from": addr}
-    tx = contract.functions["nativeMint" + suffix](amount).build_transaction(data)
+    tx = contract.functions.nativeMint(amount).build_transaction({"from": addr})
     receipt = send_transaction(w3, tx)
     assert receipt.status == 1, "expect success"
 
     # query balance through contract
-    balance_of = getattr(contract.caller, "nativeBalanceOf" + suffix)
-    assert balance_of(addr) == amount
+    assert contract.caller.nativeBalanceOf(addr) == amount
     # query balance through cosmos rpc
     cli = ethermint.cosmos_cli()
     assert cli.balance(eth_to_bech32(addr), "evm/" + contract.address) == amount
 
     # test exception revert
-    tx = contract.functions["nativeMintRevert" + suffix](amount).build_transaction(
+    tx = contract.functions.nativeMintRevert(amount).build_transaction(
         {"from": addr, "gas": 210000}
     )
     receipt = send_transaction(w3, tx)
     assert receipt.status == 0, "expect failure"
 
     # check balance don't change
-    assert balance_of(addr) == amount
+    assert contract.caller.nativeBalanceOf(addr) == amount
+    # query balance through cosmos rpc
+    cli = ethermint.cosmos_cli()
+    assert cli.balance(eth_to_bech32(addr), "evm/" + contract.address) == amount
+
+
+def test_delegate(ethermint):
+    w3 = ethermint.w3
+    addr = ADDRS["validator"]
+    amount = 100
+    _, res = deploy_contract(w3, CONTRACTS["TestBank"])
+    bank = res["contractAddress"]
+    contract, _ = deploy_contract(w3, CONTRACTS["TestBankDelegate"])
+    data = {"from": addr}
+    tx = contract.functions.nativeMint(bank, amount).build_transaction(data)
+    receipt = send_transaction(w3, tx)
+    assert receipt.status == 1, "expect success"
+
+    # query balance through contract
+    assert contract.caller.nativeBalanceOf(bank, addr) == amount
+    # query balance through cosmos rpc
+    cli = ethermint.cosmos_cli()
+    assert cli.balance(eth_to_bech32(addr), "evm/" + contract.address) == amount
+
+    # test exception revert
+    tx = contract.functions.nativeMintRevert(bank, amount).build_transaction(
+        {"from": addr, "gas": 210000}
+    )
+    receipt = send_transaction(w3, tx)
+    assert receipt.status == 0, "expect failure"
+
+    # check balance don't change
+    assert contract.caller.nativeBalanceOf(bank, addr) == amount
     # query balance through cosmos rpc
     cli = ethermint.cosmos_cli()
     assert cli.balance(eth_to_bech32(addr), "evm/" + contract.address) == amount
--- tests/integration_tests/utils.py
+++ tests/integration_tests/utils.py
@@ -33,6 +33,7 @@ TEST_CONTRACTS = {
     "Mars": "Mars.sol",
     "StateContract": "StateContract.sol",
     "TestBank": "TestBank.sol",
+    "TestBankDelegate": "TestBankDelegate.sol",
 }
 
 
