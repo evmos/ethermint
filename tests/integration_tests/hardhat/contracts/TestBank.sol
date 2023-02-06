6dfc48f30c42472856ce9336332efdf3dd120cf8
mmsqe <mavis@crypto.com>
2023-02-06 09:48:03 +0800

    align amt

--- tests/integration_tests/hardhat/contracts/TestBank.sol
+++ tests/integration_tests/hardhat/contracts/TestBank.sol
@@ -3,20 +3,20 @@ pragma solidity >0.6.6;
 
 contract TestBank {
     address constant bankContract = 0x0000000000000000000000000000000000000064;
-    function nativeMint(uint amount) public {
+    function nativeMint(uint256 amount) public {
         (bool result, bytes memory _data) = bankContract.call(abi.encodeWithSignature(
             "mint(address,uint256)", msg.sender, amount
         ));
         require(result, "native call");
     }
-    function nativeBalanceOf(address addr) public returns (uint) {
+    function nativeBalanceOf(address addr) public returns (uint256) {
         (bool result, bytes memory data) = bankContract.call(abi.encodeWithSignature(
             "balanceOf(address,address)", address(this), addr
         ));
         require(result, "native call");
-        return abi.decode(data, (uint));
+        return abi.decode(data, (uint256));
     }
-    function nativeMintRevert(uint amount) public {
+    function nativeMintRevert(uint256 amount) public {
         nativeMint(amount);
         revert("test");
     }
--- tests/integration_tests/hardhat/contracts/TestBankDelegate.sol
+++ tests/integration_tests/hardhat/contracts/TestBankDelegate.sol
@@ -2,20 +2,20 @@
 pragma solidity >0.6.6;
 
 contract TestBankDelegate {
-    function nativeMint(address _contract, uint amount) public {
+    function nativeMint(address _contract, uint256 amount) public {
         (bool result, bytes memory _data) = _contract.delegatecall(abi.encodeWithSignature(
             "nativeMint(uint256)", amount
         ));
         require(result, "native call");
     }
-    function nativeBalanceOf(address _contract, address addr) public returns (uint) {
+    function nativeBalanceOf(address _contract, address addr) public returns (uint256) {
         (bool result, bytes memory data) = _contract.delegatecall(abi.encodeWithSignature(
             "nativeBalanceOf(address)", addr
         ));
         require(result, "native call");
-        return abi.decode(data, (uint));
+        return abi.decode(data, (uint256));
     }
-    function nativeMintRevert(address _contract, uint amount) public {
+    function nativeMintRevert(address _contract, uint256 amount) public {
         nativeMint(_contract, amount);
         revert("test");
     }
