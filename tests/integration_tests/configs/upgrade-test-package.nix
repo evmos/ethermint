08435946b6f22f7dc836d72f7da76f8b1e4ea5ea
mmsqe <mavis@crypto.com>
2023-03-17 09:18:13 +0800

    keep migrate related stuff no change

--- tests/integration_tests/configs/upgrade-test-package.nix
+++ tests/integration_tests/configs/upgrade-test-package.nix
@@ -1,9 +1,9 @@
 let
   pkgs = import ../../../nix { };
-  fetchEthermint = rev: builtins.fetchTarball "https://github.com/mmsqe/ethermint/archive/${rev}.tar.gz";
+  fetchEthermint = rev: builtins.fetchTarball "https://github.com/evmos/ethermint/archive/${rev}.tar.gz";
   released = pkgs.buildGo118Module rec {
     name = "ethermintd";
-    src = fetchEthermint "0e4d41eef5f7983f84206b1bb07e88ed3a9cd44b";
+    src = fetchEthermint "d29cdad6e667f6089dfecbedd36bb8d3a2a7d025";
     subPackages = [ "cmd/ethermintd" ];
     vendorSha256 = "sha256-cQAol54b6hNzsA4Q3MP9mTqFWM1MvR5uMPrYpaoj3SY=";
     doCheck = false;
