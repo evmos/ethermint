9386c1834a02c0353abbcfb9a267e63a40732e8d
Austin Chandra <austinchandra@berkeley.edu>
2023-04-10 14:49:53 -0700

    Update licenses for Ethermint

--- ethereum/eip712/encoding_legacy.go
+++ ethereum/eip712/encoding_legacy.go
@@ -1,18 +1,18 @@
-// Copyright 2022 Evmos Foundation
-// This file is part of the Evmos Network packages.
+// Copyright 2023 Evmos Foundation
+// This file is part of Evmos' Ethermint library.
 //
-// Evmos is free software: you can redistribute it and/or modify
+// The Ethermint library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The Evmos packages are distributed in the hope that it will be useful,
+// The Ethermint library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the Evmos packages. If not, see https://github.com/evmos/evmos/blob/main/LICENSE
+// along with the Ethermint library. If not, see https://github.com/evmos/ethermint/blob/main/LICENSE
 package eip712
 
 import (
