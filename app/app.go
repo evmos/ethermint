d579f73f3924cd86b121afde28f0a017b00aa1fa
mmsqe <mavis@crypto.com>
2023-06-05 10:35:39 +0800

    set chainId

--- app/app.go
+++ app/app.go
@@ -302,6 +302,8 @@ func NewEthermintApp(
 		app.SetPrepareProposal(handler.PrepareProposalHandler())
 		app.SetProcessProposal(handler.ProcessProposalHandler())
 	})
+	// Setup chainId
+	baseAppOptions = append(baseAppOptions, baseapp.SetChainID("ethermint_9000-1"))
 	bApp := baseapp.NewBaseApp(
 		appName,
 		logger,
